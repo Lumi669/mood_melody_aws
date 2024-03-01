f5fe599c3dfdd9d9462b95345d07204f9b5fcabd
cmjiwa <wangjinghuan@yahoo.com>
2024-03-01 18:45:11 +0200

    Test workflow

--- backend/app.py
+++ backend/app.py
@@ -40,7 +40,7 @@ def create_tables():
 
 @app.route('/test', methods=['GET'])
 def test():
-    return jsonify({'message': 'The server is running ....===ooooookkkkkkkpppppkk'})
+    return jsonify({'message': 'The server is running ...'})
 
 @app.route('/api/musics', methods=['POST'])
 def create_music():
