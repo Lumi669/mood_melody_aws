87690a4e84dbc5a92bef92c81a8d55a1d313bbae
cmjiwa <wangjinghuan@yahoo.com>
2024-03-03 23:36:24 +0200

    configure next.config.js and use dynamic port in frontend/package.json file and add debug code in workflow

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,10 +1,23 @@
-/** @type {import('next').NextConfig} */
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   output: "standalone",
+
+// };
+
+// module.exports = nextConfig;
+
+const path = require("path");
+
 const nextConfig = {
   reactStrictMode: true,
   output: "standalone",
-  // experimental: {
-  //   outputStandalone: true,
-  // },
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    // Resolve the '@' alias to the 'src' directory
+    config.resolve.alias["@"] = path.resolve(__dirname, "src");
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;
