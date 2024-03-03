c20909f2b8216016eb3af6a1248cba468aaae56c
cmjiwa <wangjinghuan@yahoo.com>
2024-03-03 19:24:44 +0200

    Frontend can use music url to play music

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,6 +1,8 @@
 // import Image from "next/image";
 import { Inter } from 'next/font/google';
 
+import Display from '../components/Display';
+
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
@@ -9,6 +11,7 @@ export default function Home() {
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
       <div>Happy weekend :D Satuday !</div>
+      <Display>Hello</Display>
     </main>
   );
 }
