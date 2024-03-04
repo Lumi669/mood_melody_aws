f6eb5eeec65770c0406d1691be174fde697bae74
cmjiwa <wangjinghuan@yahoo.com>
2024-03-05 00:27:04 +0200

    Data flows from heroku backend to frontend api/musics.ts to index.tsx

--- backend/app.py
+++ backend/app.py
@@ -43,7 +43,7 @@ def create_tables():
 
 @app.route('/test', methods=['GET'])
 def test():
-    return jsonify({'message': 'The server is running ...'})
+    return jsonify({'message': 'The server is running ooo..hhh.'})
 
 @app.route('/api/musics', methods=['POST'])
 def create_music():
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,18 +1,44 @@
 // import Image from "next/image";
 import { Inter } from "next/font/google";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-// import Display from "../components/Display";
 import Ui from "@/components/Ui";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("/api/musics");
+        setData(response.data);
+        console.log("data === ", data);
+      } catch (error) {
+        console.error("Error fetching data: ", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    console.log("Updated data === ", data);
+  }, [data]);
+
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
-      <div>Happy weekend :D Sunday!</div>
+      <div>Happy weekend :D Sunday kkkk!</div>
       <Ui>Hello</Ui>
+      <div>
+        {data.map((item, index) => (
+          <div key={index}>{item.id}</div>
+        ))}
+      </div>
     </main>
   );
 }
