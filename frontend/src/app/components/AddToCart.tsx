f73cb08c302edf81869adf0167e1645f967a895b
cmjiwa <wangjinghuan@yahoo.com>
2024-05-08 13:24:40 +0300

    Use @apply

--- frontend/src/app/components/AddToCart.tsx
+++ frontend/src/app/components/AddToCart.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const AddToCart = () => {
   return (
-    <div className="font-jersey font-bold-text-2xl">
+    <div className="test">
       <button onClick={() => console.log("click button")}>
         Add to Cart Whereas reco
       </button>
