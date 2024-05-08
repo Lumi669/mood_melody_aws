fc583068353146e3a965081e38d056e7dc042b4e
cmjiwa <wangjinghuan@yahoo.com>
2024-05-08 21:50:41 +0300

    Add a file for types

---
+++ frontend/src/app/types/type.ts
@@ -0,0 +1,15 @@
+export interface Music {
+  id: string;
+  name: string;
+  url: string;
+  ctg: string;
+  mood: "happy" | "sad";
+}
+
+export interface Image {
+  id: string;
+  name: string;
+  url: string;
+  ctg: string;
+  mood: "happy" | "sad";
+}
