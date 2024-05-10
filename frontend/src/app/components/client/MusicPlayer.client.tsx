1868cf780a7be4972007900643914a7f02d7bc21
cmjiwa <wangjinghuan@yahoo.com>
2024-05-10 17:50:30 +0300

    Implement progress bar for music

--- frontend/src/app/components/client/MusicPlayer.client.tsx
+++ frontend/src/app/components/client/MusicPlayer.client.tsx
@@ -1,10 +1,15 @@
 // MusicPlayer.tsx
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useMedia } from "../../context/MediaContext";
 
 const MusicPlayer: React.FC = () => {
   const { mediaData } = useMedia();
   const [audio, setAudio] = useState<HTMLAudioElement | null>(null);
+
+  //for progress bar
+  const [currentTime, setCurrentTime] = useState<number>(0);
+  //for progress bar
+  const [duration, setDuration] = useState<number>(0);
 
   const playMusic = (mood: "happy" | "sad") => {
     const filteredSongs = mediaData.filter((song) => song.mood === mood);
@@ -14,16 +19,41 @@ const MusicPlayer: React.FC = () => {
       audio.pause();
     }
     const newAudio = new Audio(randomSong.url);
+
+    //for progress bar
+    newAudio.addEventListener("loadedmetadata", () => {
+      setDuration(newAudio.duration);
+    });
+    //for progress bar
+    newAudio.addEventListener("timeupdate", () => {
+      setCurrentTime(newAudio.currentTime);
+    });
+
     setAudio(newAudio);
     newAudio.play();
   };
 
   const stopMusic = () => {
     if (audio) {
+      console.log("audio ===== ", audio);
       audio.pause();
       audio.currentTime = 0;
     }
   };
+
+  useEffect(() => {
+    return () => {
+      if (audio) {
+        audio.pause();
+        audio.removeEventListener("loadedmetadata", () =>
+          setDuration(audio.duration),
+        );
+        audio.removeEventListener("timeupdate", () =>
+          setCurrentTime(audio.currentTime),
+        );
+      }
+    };
+  }, [audio]);
 
   return (
     <>
@@ -51,6 +81,8 @@ const MusicPlayer: React.FC = () => {
       >
         Stop
       </button>
+
+      {audio && <progress value={currentTime} max={duration}></progress>}
     </>
   );
 };
