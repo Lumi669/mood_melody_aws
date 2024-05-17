d9da8a80019e6f6ac211cb745b19a5814e573480
cmjiwa <wangjinghuan@yahoo.com>
2024-05-17 12:56:04 +0300

    Implement images POST, GET, DELETE

--- newbackend/index.js
+++ newbackend/index.js
@@ -70,6 +70,36 @@ app.delete('/api/musics', async (req, res) => {
   }
 });
 
+// images
+
+app.post('/api/images', async (req, res) => {
+  try {
+    const newImages = await Image.bulkCreate(req.body, { validate: true });
+    res.status(201).json(newImages);
+  } catch (error) {
+    res.status(500).json({ message: 'Error creating image', error: error.message });
+  }
+});
+
+app.get('/api/images', async (req, res) => {
+  try {
+    const images = await Image.findAll();
+    res.status(200).json(images);
+  } catch (error) {
+    res.status(500).json({ message: 'error getting images', error: error.message });
+  }
+});
+
+app.delete('/api/images', async (req, res) => {
+  try {
+    await Image.destroy({ where: {} });
+    res.status(200).json({ message: 'All data deleted successfully' });
+  } catch (error) {
+    res.status(500).json({ message: 'Error deleting data', error: error.message });
+  }
+});
+
+
 // Additional routes for images similarly...
 
 app.get('/', (req, res) => {
