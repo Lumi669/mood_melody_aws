1f043f1e8a50dd9be31fab6ac1d1fcbeb287dd5e
cmjiwa <wangjinghuan@yahoo.com>
2024-05-17 14:38:32 +0300

    Fetch data from loclal database instead of heroku python backend app

--- frontend/src/app/utils/fetchAllMusicWithImages.ts
+++ frontend/src/app/utils/fetchAllMusicWithImages.ts
@@ -1,14 +1,23 @@
 import { Music, Image } from "../types/type";
 export const fetchAllMusicWithImages = async () => {
   const musicResponse = await fetch(
-    "https://mood-melody-backend-860ac0ad6346.herokuapp.com/api/musics",
+    "http://localhost:4000/api/musics",
   );
   const imageResponse = await fetch(
-    "https://mood-melody-backend-860ac0ad6346.herokuapp.com/api/images",
+    "http://localhost:4000/api/images",
   );
+  // const musicResponse = await fetch(
+  //   "https://mood-melody-backend-860ac0ad6346.herokuapp.com/api/musics",
+  // );
+  // const imageResponse = await fetch(
+  //   "https://mood-melody-backend-860ac0ad6346.herokuapp.com/api/images",
+  // );
 
   const musicData = await musicResponse.json();
   const imageData = await imageResponse.json();
+
+  console.log("musicData from context === ", musicData);
+  console.log("imageData from context === ", imageData);
 
   // Match music and image records based on common identifier
   const matchedData = musicData.map((music: Music) => {
