5f15cc9aa6f192d31cb8293947be0dd3020ea874
cmjiwa <wangjinghuan@yahoo.com>
2024-05-21 16:47:26 +0300

    Nodejs backend can build and run docker image now

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-# Example of a Node.js entrypoint script
 
 # Run database migrations (modify as needed for your setup)
 echo "Running database migrations"
@@ -7,4 +6,4 @@ npx sequelize db:migrate
 
 # Start the Node.js application on Heroku's assigned port or a default port
 echo "Starting Node.js app on port $PORT"
-exec node index.js --port=${PORT:-4000}
+exec node dist/index.js --port=${PORT:-4000}
