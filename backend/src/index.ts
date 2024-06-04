8c3c433bf22c52350b46ab811bfafb732c80b161
cmjiwa <wangjinghuan@yahoo.com>
2024-06-04 14:28:37 +0300

    Correct debug code

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -91,7 +91,7 @@ sequelize.sync();
 // Routes
 app.get("/test", (req: Request, res: Response) => {
   res.json({
-    message: "The server is running ooo..hhhookkfHkkjkm66n89kk8ssnm=.",
+    message: "The server is running ooo..hhhookkfHkkjkm66n8j9kk8ssnm=.",
   });
 });
 
