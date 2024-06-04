c07c0dc6b74b92db073d03cd499ea423ff8766c6
cmjiwa <wangjinghuan@yahoo.com>
2024-06-04 13:17:23 +0300

    Modify function of create_or_update_resource_and_method

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -91,7 +91,7 @@ sequelize.sync();
 // Routes
 app.get("/test", (req: Request, res: Response) => {
   res.json({
-    message: "The server is running ooo..hhhookkfHkkjkmn88ssnm=.",
+    message: "The server is running ooo..hhhookkfHkkjkmn8kk8ssnm=.",
   });
 });
 
