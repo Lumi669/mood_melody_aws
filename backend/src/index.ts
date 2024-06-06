68ba27376f2a429c46299aa75ccfdbbeb2780018
cmjiwa <wangjinghuan@yahoo.com>
2024-06-06 12:09:56 +0300

    Modify checkout repo

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -91,7 +91,7 @@ sequelize.sync();
 // Routes
 app.get("/test", (req: Request, res: Response) => {
   res.json({
-    message: "The server is running ooo..hhhook==dd==???jjj?==h=pp",
+    message: "The server is running ooo..hhhook==dd==?gg??jjj?==h=pp",
   });
 });
 
