91b805eca64b26d0320854dc4c642083c64e9d0f
cmjiwa <wangjinghuan@yahoo.com>
2024-06-30 22:55:15 +0300

    Try to solve TypeError: Failed to parse URL from ?cacheBust=1719771148769

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -2,6 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   output: "standalone",
+  env: {
+    NEXT_PUBLIC_API_URL_0: process.env.NEXT_PUBLIC_API_URL_0,
+    NEXT_PUBLIC_API_URL_1: process.env.NEXT_PUBLIC_API_URL_1,
+    NEXT_PUBLIC_API_URL_2: process.env.NEXT_PUBLIC_API_URL_2,
+    NEXT_PUBLIC_API_URL_3: process.env.NEXT_PUBLIC_API_URL_3,
+  },
 };
 
 export default nextConfig;
--- frontend/utils/fetchAllMusicWithImages.ts
+++ frontend/utils/fetchAllMusicWithImages.ts
@@ -5,29 +5,47 @@ import { apiUrls } from "../config/apiConfig";
 export const fetchAllMusicWithImages = async () => {
   console.log("apiUrls.images ====== ", apiUrls.images);
   console.log("apiUrls.musics ====== ", apiUrls.musics);
-  const musicResponse = await fetch(
-    `${apiUrls.musics}?cacheBust=${Date.now()}`,
-  );
-  const imageResponse = await fetch(
-    `${apiUrls.images}?cacheBust=${Date.now()}`,
-  );
-  // const imageResponse = await fetch(
-  //   `http://localhost:4000/api/images?cacheBust=${Date.now()}`,
-  // );
 
-  const musicData = await musicResponse.json();
-  const imageData = await imageResponse.json();
-
-  console.log("musicData from context === ", musicData);
-  console.log("imageData from context === ", imageData);
-
-  // Match music and image records based on common identifier
-  const matchedData = musicData.map((music: Music) => {
-    const matchingImage = imageData.find(
-      (image: Image) => image.ctg === music.ctg,
+  try {
+    const musicResponse = await fetch(
+      `${apiUrls.musics}?cacheBust=${Date.now()}`,
     );
-    return { ...music, imgUrl: matchingImage ? matchingImage.url : "" };
-  });
+    const imageResponse = await fetch(
+      `${apiUrls.images}?cacheBust=${Date.now()}`,
+    );
+    // const imageResponse = await fetch(
+    //   `http://localhost:4000/api/images?cacheBust=${Date.now()}`,
+    // );
 
-  return matchedData;
+    if (!musicResponse.ok) {
+      throw new Error(
+        `Failed to fetch music data: ${musicResponse.statusText}`,
+      );
+    }
+
+    if (!imageResponse.ok) {
+      throw new Error(
+        `Failed to fetch image data: ${imageResponse.statusText}`,
+      );
+    }
+
+    const musicData = await musicResponse.json();
+    const imageData = await imageResponse.json();
+
+    console.log("musicData from context === ", musicData);
+    console.log("imageData from context === ", imageData);
+
+    // Match music and image records based on common identifier
+    const matchedData = musicData.map((music: Music) => {
+      const matchingImage = imageData.find(
+        (image: Image) => image.ctg === music.ctg,
+      );
+      return { ...music, imgUrl: matchingImage ? matchingImage.url : "" };
+    });
+
+    return matchedData;
+  } catch (error) {
+    console.error("Error fetching data: ", error);
+    throw error;
+  }
 };
