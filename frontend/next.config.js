598c7df9f489c0993117b9d9dc9f5e8e68bd04c1
cmjiwa <wangjinghuan@yahoo.com>
2024-06-30 20:41:23 +0300

    Modify next.config.js => putput: 'standalone'

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -7,21 +7,28 @@
 
 // module.exports = nextConfig;
 
-const path = require("path");
+// const path = require("path");
 
+// const nextConfig = {
+//   reactStrictMode: true,
+//   // output: "standalone",
+//   output: "export",
+//   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+//     // Resolve the '@' alias to the 'src' directory
+//     config.resolve.alias["@"] = path.resolve(__dirname, "src");
+
+//     return config;
+//   },
+//   images: {
+//     domains: ["mood-melody.s3.eu-north-1.amazonaws.com"],
+//   },
+// };
+
+// module.exports = nextConfig;
+
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  // output: "standalone",
-  output: "export",
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    // Resolve the '@' alias to the 'src' directory
-    config.resolve.alias["@"] = path.resolve(__dirname, "src");
-
-    return config;
-  },
-  images: {
-    domains: ["mood-melody.s3.eu-north-1.amazonaws.com"],
-  },
+  output: "standalone",
 };
 
-module.exports = nextConfig;
+export default nextConfig;
