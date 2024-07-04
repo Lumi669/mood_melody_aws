c2569d32c9f0285ce1efa0e27be279ad08e60039
cmjiwa <wangjinghuan@yahoo.com>
2024-07-04 22:13:31 +0300

    Add moodMelodyAPIGatewayPatchPermissionForCloudformationServiceRolePolicy-040724 to CloudFormationServiceRole-jinghuan-040724

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -91,7 +91,7 @@ sequelize.sync();
 // Routes
 app.get("/test", (req: Request, res: Response) => {
   res.json({
-    message: "The server is running bla bla bla ....=======...",
+    message: "The server is running bla bla bla ....======kkk=...",
   });
 });
 
