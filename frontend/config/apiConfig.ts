a83dc5cfd3456bf0e4097c4370c80e2820d0b86c
cmjiwa <wangjinghuan@yahoo.com>
2024-07-09 17:16:23 +0300

    Fix apiUrls.images=/api/musics error identified in frontend github actions

--- frontend/config/apiConfig.ts
+++ frontend/config/apiConfig.ts
@@ -3,12 +3,12 @@ interface ApiUrls {
   base: string;
   images: string;
   musics: string;
-  test: string;
+  // test: string;
 }
 
 export const apiUrls: ApiUrls = {
   base: process.env.NEXT_PUBLIC_API_URL_0 || "",
   images: process.env.NEXT_PUBLIC_API_URL_1 || "",
   musics: process.env.NEXT_PUBLIC_API_URL_2 || "",
-  test: process.env.NEXT_PUBLIC_API_URL_3 || "",
+  // test: process.env.NEXT_PUBLIC_API_URL_3 || "",
 };
