1658dfa520ed86c07a6376718d4a708dfd1b4382
cmjiwa <wangjinghuan@yahoo.com>
2024-07-09 19:15:41 +0300

    Continuosly fixt error of last commit's message described

--- frontend/config/apiConfig.ts
+++ frontend/config/apiConfig.ts
@@ -1,14 +1,14 @@
 // src/config/apiConfig.ts
 interface ApiUrls {
-  base: string;
+  //   base: string;
   images: string;
   musics: string;
   // test: string;
 }
 
 export const apiUrls: ApiUrls = {
-  base: process.env.NEXT_PUBLIC_API_URL_0 || "",
-  images: process.env.NEXT_PUBLIC_API_URL_1 || "",
-  musics: process.env.NEXT_PUBLIC_API_URL_2 || "",
+  // base: process.env.NEXT_PUBLIC_API_URL_0 || "",
+  images: process.env.NEXT_PUBLIC_API_URL_0 || "",
+  musics: process.env.NEXT_PUBLIC_API_URL_1 || "",
   // test: process.env.NEXT_PUBLIC_API_URL_3 || "",
 };
