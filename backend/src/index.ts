3bc1d1ec40653304d36894b6434f9e52de8da964
cmjiwa <wangjinghuan@yahoo.com>
2024-07-16 13:22:44 +0300

    Reupload trigger-frontend-build.zip suspecting uploaded wrong filelast commit

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -196,7 +196,7 @@ app.post("/submit", (req, res) => {
 });
 
 app.get("/", (req: Request, res: Response) => {
-  res.json({ message: "Welcome to the Music Mood API!=====yy999oohellopp" });
+  res.json({ message: "Welcome to the Music Mood API!=====yy999oohellokkpp" });
 });
 
 // Start server locally if not in Lambda
