e9b37e3d0fb02a8a4a33e0b7492980dd25797f7b
cmjiwa <wangjinghuan@yahoo.com>
2024-07-20 17:16:53 +0300

    Not use stage name prod in Links, deleted the old frontend stack and redeploy frontend

--- frontend/components/Nav.tsx
+++ frontend/components/Nav.tsx
@@ -5,16 +5,16 @@ const Nav: React.FC = () => {
   return (
     <nav className="sticky top-0 z-50 bg-white shadow-md">
       <div className="container mx-auto flex justify-between items-center p-4">
-        <Link href="/prod" className="text-lg font-semibold">
+        <Link href="/" className="text-lg font-semibold">
           Home
         </Link>
-        <Link href="/prod/allmusic" className="text-lg font-semibold">
+        <Link href="/allmusic" className="text-lg font-semibold">
           All Music
         </Link>
-        <Link href="/prod/mixtape" className="text-lg font-semibold">
+        <Link href="/mixtape" className="text-lg font-semibold">
           MixtapeY
         </Link>
-        <Link href="/prod/live" className="text-lg font-semibold">
+        <Link href="/live" className="text-lg font-semibold">
           Live
         </Link>
       </div>
--- frontend/middleware.js
+++ frontend/middleware.js
@@ -1,15 +1,15 @@
-import { NextResponse } from "next/server";
+// import { NextResponse } from "next/server";
 
-export function middleware(req) {
-  const url = req.nextUrl.clone();
-  console.log("url from middleware ====== ", url);
+// export function middleware(req) {
+//   const url = req.nextUrl.clone();
+//   console.log("url from middleware ====== ", url);
 
-  // Redirect to include the base path if missing
-  if (!url.pathname.startsWith("/prod")) {
-    url.pathname = `/prod${url.pathname}`;
-    console.log("url after adding stage prod ====== ", url);
-    return NextResponse.redirect(url);
-  }
+//   // Redirect to include the base path if missing
+//   if (!url.pathname.startsWith("/prod")) {
+//     url.pathname = `/prod${url.pathname}`;
+//     console.log("url after adding stage prod ====== ", url);
+//     return NextResponse.redirect(url);
+//   }
 
-  return NextResponse.next();
-}
+//   return NextResponse.next();
+// }
