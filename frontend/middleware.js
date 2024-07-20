ab2251d96cb9ab305df3dcf0ae8e7c8d9e29814f
cmjiwa <wangjinghuan@yahoo.com>
2024-07-20 16:31:38 +0300

    Prind url from middleware.js

--- frontend/middleware.js
+++ frontend/middleware.js
@@ -2,10 +2,12 @@ import { NextResponse } from "next/server";
 
 export function middleware(req) {
   const url = req.nextUrl.clone();
+  console.log("url from middleware ====== ", url);
 
   // Redirect to include the base path if missing
   if (!url.pathname.startsWith("/prod")) {
     url.pathname = `/prod${url.pathname}`;
+    console.log("url after adding stage prod === ", url);
     return NextResponse.redirect(url);
   }
 
