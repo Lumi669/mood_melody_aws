2d1cec0da155c0b3ce4c856f27520f80c807fadd
cmjiwa <wangjinghuan@yahoo.com>
2024-07-23 17:28:00 +0300

    Modify run.sh to debugi

--- frontend/run.sh
+++ frontend/run.sh
@@ -1,15 +1,12 @@
 #!/bin/bash -x
 
-# Check if /tmp/cache exists, if not, create it
 if [ ! -d "/tmp/cache" ]; then
   echo "Creating /tmp/cache directory"
   mkdir -p /tmp/cache
 fi
 
-# Log the environment variables
 echo "Environment Variables:"
 env
 
-# Start the Next.js server
 echo "Starting Next.js server"
-exec node server.js
+exec node server.js 2>&1 | tee /tmp/server.log
