b738b592ea47f1cc408f56258a7d9f3dbc3362ef
cmjiwa <wangjinghuan@yahoo.com>
2024-07-23 16:32:37 +0300

    Modify frontend/server.js

--- frontend/server.js
+++ frontend/server.js
@@ -10,14 +10,6 @@ const server = express();
 app
   .prepare()
   .then(() => {
-    server.get("/prod/live", (req, res) => {
-      res.json({ message: "Live page" });
-    });
-
-    server.get("/prod/allmusic", (req, res) => {
-      res.json({ message: "All music page" });
-    });
-
     server.all("*", (req, res) => {
       return handle(req, res);
     });
@@ -25,6 +17,6 @@ app
     module.exports.handler = serverless(server);
   })
   .catch((err) => {
-    console.error("Error during app preparation ====== ", err);
+    console.error("Error during app preparation:", err);
     process.exit(1); // Exit the process if there's an error
   });
