8b02f99530cd073d85c3990b60d65111861098cd
cmjiwa <wangjinghuan@yahoo.com>
2024-07-23 16:16:56 +0300

    Modify frontend/server.js

--- frontend/server.js
+++ frontend/server.js
@@ -1,4 +1,3 @@
-// server.js
 const express = require("express");
 const serverless = require("serverless-http");
 const next = require("next");
@@ -8,18 +7,24 @@ const handle = app.getRequestHandler();
 
 const server = express();
 
-app.prepare().then(() => {
-  server.get("/prod/live", (req, res) => {
-    res.json({ message: "Live page" });
-  });
+app
+  .prepare()
+  .then(() => {
+    server.get("/prod/live", (req, res) => {
+      res.json({ message: "Live page" });
+    });
 
-  server.get("/prod/allmusic", (req, res) => {
-    res.json({ message: "Live page" });
-  });
+    server.get("/prod/allmusic", (req, res) => {
+      res.json({ message: "All music page" });
+    });
 
-  server.all("*", (req, res) => {
-    return handle(req, res);
-  });
+    server.all("*", (req, res) => {
+      return handle(req, res);
+    });
 
-  module.exports.handler = serverless(server);
-});
+    module.exports.handler = serverless(server);
+  })
+  .catch((err) => {
+    console.error("Error during app preparation ====== ", err);
+    process.exit(1); // Exit the process if there's an error
+  });
