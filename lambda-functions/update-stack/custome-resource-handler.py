d3c844f64d899f57eff76af620627c656c9ff4b6
cmjiwa <wangjinghuan@yahoo.com>
2024-08-05 16:47:25 +0300

    Create and use custom resource to control code chanes in backend can be auto reflected in the deployed app

---
+++ lambda-functions/update-stack/custome-resource-handler.py
@@ -0,0 +1,14 @@
+import json
+import boto3
+
+def handler(event, context):
+    print("Custom resource event: ", event)
+    response = {
+        'Status': 'SUCCESS',
+        'PhysicalResourceId': context.log_stream_name,
+        'StackId': event['StackId'],
+        'RequestId': event['RequestId'],
+        'LogicalResourceId': event['LogicalResourceId'],
+        'Data': {'Message': 'Triggered'}
+    }
+    return response
