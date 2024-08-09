c9389e0819c3831d88a4347074c8ab5b57a85e15
cmjiwa <wangjinghuan@yahoo.com>
2024-08-09 20:57:00 +0300

    Add mood-melody-ssm-GetParameters-policy to CloudFormationServiceRole-jinghuan-040724 role

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -197,7 +197,7 @@ app.post("/submit", (req, res) => {
 
 app.get("/", (req: Request, res: Response) => {
   res.json({
-    message: "Welcome to the Music Mood API!=====yy99000ttp99?",
+    message: "Welcome to the Music Mood API!=====yy99tp99?",
   });
 });
 
