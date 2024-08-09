00d0ad5812735cd49523333a06d1e551fe48a7bb
cmjiwa <wangjinghuan@yahoo.com>
2024-08-09 16:06:47 +0300

    Test custom resource

--- lambda-functions/update-stack/cfnresponse.py
+++ lambda-functions/update-stack/cfnresponse.py
@@ -1,3 +1,41 @@
+# import json
+# import urllib3
+
+# http = urllib3.PoolManager()
+
+# SUCCESS = "SUCCESS"
+# FAILED = "FAILED"
+
+# def send(event, context, responseStatus, responseData, physicalResourceId=None, noEcho=False, reason=None):
+#     responseUrl = event['ResponseURL']
+
+#     responseBody = {
+#         'Status': responseStatus,
+#         'Reason': reason or 'See the details in CloudWatch Log Stream: ' + context.log_stream_name,
+#         'PhysicalResourceId': physicalResourceId or context.log_stream_name,
+#         'StackId': event['StackId'],
+#         'RequestId': event['RequestId'],
+#         'LogicalResourceId': event['LogicalResourceId'],
+#         'NoEcho': noEcho,
+#         'Data': responseData
+#     }
+
+#     json_responseBody = json.dumps(responseBody)
+#     print("json_responseBody ==== ", json_responseBody)
+
+#     headers = {
+#         'content-type': '',
+#         'content-length': str(len(json_responseBody))
+#     }
+
+#     try:
+#         response = http.request('PUT', responseUrl, body=json_responseBody, headers=headers)
+#         print("Status code: ", response.status)
+#         print("Status message: ", response.reason)
+#     except Exception as e:
+#         print("Failed to send response to CloudFormation: ", e)
+
+
 import json
 import urllib3
 
@@ -21,7 +59,6 @@ def send(event, context, responseStatus, responseData, physicalResourceId=None, 
     }
 
     json_responseBody = json.dumps(responseBody)
-    print("json_responseBody ==== ", json_responseBody)
 
     headers = {
         'content-type': '',
@@ -29,6 +66,8 @@ def send(event, context, responseStatus, responseData, physicalResourceId=None, 
     }
 
     try:
+        print("Response body being sent: ", json_responseBody)
+        
         response = http.request('PUT', responseUrl, body=json_responseBody, headers=headers)
         print("Status code: ", response.status)
         print("Status message: ", response.reason)
