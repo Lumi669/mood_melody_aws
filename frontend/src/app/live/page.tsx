906baadbd87f21540b22940b90f37a9288450be5
cmjiwa <wangjinghuan@yahoo.com>
2024-08-10 21:38:01 +0300

    Delete custom resource handler

--- frontend/src/app/live/page.tsx
+++ frontend/src/app/live/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Liveplay = () => {
-  return <div>Liveplay: now it is playing xxxxxx======6ppp?????==</div>;
+  return <div>Liveplay: now it is playing xxxxxx======6ppp?????==999</div>;
 };
 
 export default Liveplay;
