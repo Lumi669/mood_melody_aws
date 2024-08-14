5cb579ae778e0ae4fe60ede272976c5dbe5fd83e
cmjiwa <wangjinghuan@yahoo.com>
2024-08-14 16:14:52 +0300

    Comment out server.js

--- frontend/server.js
+++ frontend/server.js
@@ -118,19 +118,19 @@
 //     process.exit(1);
 //   });
 
-const { createServer } = require("http");
-const next = require("next");
+// const { createServer } = require("http");
+// const next = require("next");
 
-const port = process.env.PORT || 8080;
-const dev = process.env.NODE_ENV !== "production";
-const app = next({ dev });
-const handle = app.getRequestHandler();
+// const port = process.env.PORT || 8080;
+// const dev = process.env.NODE_ENV !== "production";
+// const app = next({ dev });
+// const handle = app.getRequestHandler();
 
-app.prepare().then(() => {
-  createServer((req, res) => {
-    handle(req, res);
-  }).listen(port, (err) => {
-    if (err) throw err;
-    console.log(`> Ready on http://localhost:${port}`);
-  });
-});
+// app.prepare().then(() => {
+//   createServer((req, res) => {
+//     handle(req, res);
+//   }).listen(port, (err) => {
+//     if (err) throw err;
+//     console.log(`> Ready on http://localhost:${port}`);
+//   });
+// });
