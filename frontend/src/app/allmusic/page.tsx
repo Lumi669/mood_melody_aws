3cbae67f18338cb6daca9213887aa3db4c87ae18
cmjiwa <wangjinghuan@yahoo.com>
2024-08-23 17:18:47 +0300

    Distinguish the playing music image with the idle ones for all music page

--- frontend/src/app/allmusic/page.tsx
+++ frontend/src/app/allmusic/page.tsx
@@ -1,3 +1,43 @@
+// import React from "react";
+// import { MusicWithImage } from "types/type";
+// import { fetchAllMusicWithImages } from "../../../utils/fetchAllMusicWithImages";
+// import ClientContextUpdater from "../../../components/ClientContextUpdater";
+// import ClientInteractivityWrapper from "../../../components/ClientInteractivityWrapper";
+// import CustomImage from "components/CustomImage";
+
+// export default async function AllMusicPage() {
+//   // Fetch music and image data server-side
+//   const matchedData = await fetchAllMusicWithImages();
+//   console.log("matchedData from allmusic page == ", matchedData);
+
+//   return (
+//     <div>
+//       <h1>All Music</h1>
+//       <ul>
+//         {matchedData.map((item: MusicWithImage) => (
+//           <li key={item.id} className="relative h-96 w-full max-w-4xl mx-auto">
+//             <h2>{item.name}</h2>
+
+//             <CustomImage
+//               src={item.imgUrl}
+//               alt={item.name}
+//               layout="responsive"
+//               objectFit="cover"
+//               dataUrl={item.url}
+//               className="cursor-pointer"
+//               width={500}
+//               height={500}
+//             />
+//           </li>
+//         ))}
+//       </ul>
+//       <ClientContextUpdater initialData={matchedData} />
+//       <ClientInteractivityWrapper />
+//     </div>
+//   );
+// }
+
+// allmusic/page.tsx
 import React from "react";
 import { MusicWithImage } from "types/type";
 import { fetchAllMusicWithImages } from "../../../utils/fetchAllMusicWithImages";
@@ -8,23 +48,21 @@ import CustomImage from "components/CustomImage";
 export default async function AllMusicPage() {
   // Fetch music and image data server-side
   const matchedData = await fetchAllMusicWithImages();
-  console.log("matchedData from allmusic page == ", matchedData);
 
   return (
     <div>
-      <h1>All Music</h1>
+      <h1 className="text-2xl font-bold mb-4">All Music</h1>
       <ul>
         {matchedData.map((item: MusicWithImage) => (
           <li key={item.id} className="relative h-96 w-full max-w-4xl mx-auto">
-            <h2>{item.name}</h2>
-
+            <h2 className="text-xl font-semibold">{item.name}</h2>
             <CustomImage
               src={item.imgUrl}
               alt={item.name}
               layout="responsive"
               objectFit="cover"
               dataUrl={item.url}
-              className="cursor-pointer"
+              className="cursor-pointer rounded-lg transition-all duration-300"
               width={500}
               height={500}
             />
