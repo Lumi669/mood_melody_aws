534d9bec6a5aa7366f79536b5caf5be25a1a4cfa
cmjiwa <wangjinghuan@yahoo.com>
2024-08-23 21:58:20 +0300

    Get rid of old commented-out codes

--- frontend/components/ClientContextUpdater.tsx
+++ frontend/components/ClientContextUpdater.tsx
@@ -1,22 +1,3 @@
-// "use client"; // Mark this file as a client component
-// import React, { useEffect } from "react";
-// import { useMedia } from "../context/MediaContext";
-
-// const ClientContextUpdater: React.FC<{ initialData: any[] }> = ({
-//   initialData,
-// }) => {
-//   const { setMediaData } = useMedia();
-
-//   useEffect(() => {
-//     setMediaData(initialData);
-//   }, [initialData, setMediaData]);
-
-//   return null; // No need to render anything
-// };
-
-// export default ClientContextUpdater;
-
-// ClientContextUpdater.tsx
 "use client";
 
 import React, { useEffect } from "react";
