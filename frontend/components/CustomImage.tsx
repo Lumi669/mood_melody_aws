44de0059646fb9c6d8d018a5906ff850d63a8bae
cmjiwa <wangjinghuan@yahoo.com>
2024-08-26 23:19:51 +0300

    Change SVG attributes to camelCase because React us it this way

--- frontend/components/CustomImage.tsx
+++ frontend/components/CustomImage.tsx
@@ -133,13 +133,13 @@ const CustomImage: React.FC<CustomImageProps> = ({
           xmlns="http://www.w3.org/2000/svg"
           fill="white"
           viewBox="0 0 24 24"
-          stroke-width="1.5"
+          strokeWidth="1.5"
           stroke="currentColor"
           className="absolute inset-0 m-auto w-12 h-12 text-white"
         >
           <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeLinecap="round"
+            strokeLinejoin="round"
             d="M6 4.5v15m12-15v15"
           />
         </svg>
@@ -148,13 +148,13 @@ const CustomImage: React.FC<CustomImageProps> = ({
           xmlns="http://www.w3.org/2000/svg"
           fill="white"
           viewBox="0 0 24 24"
-          stroke-width="1.5"
+          strokeWidth="1.5"
           stroke="currentColor"
           className="absolute inset-0 m-auto w-12 h-12 text-white"
         >
           <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeLinecap="round"
+            strokeLinejoin="round"
             d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.347a1.125 1.125 0 0 1 0 1.972l-11.54 6.347a1.125 1.125 0 0 1-1.667-.986V5.653Z"
           />
         </svg>
