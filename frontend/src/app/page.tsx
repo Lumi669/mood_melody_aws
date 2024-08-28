e84a23c933c73c25a26ba935629fcc76a313e66d
cmjiwa <wangjinghuan@yahoo.com>
2024-08-28 15:47:51 +0300

    Redeploy to see wheather log works

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -11,7 +11,7 @@ const Home: React.FC = async () => {
   return (
     <div>
       <h1 className="m-20 p-20 bg-yellow-100 text-4xl font-mono text-green-500 font-bold tracking-wide text-center">
-        Hei, How do you feel today :) ?
+        Hi there, How do you feel today :D ?
       </h1>
       <ClientWrapperForMusicPlayer />
       <ClientContextUpdater initialData={matchedData} />
