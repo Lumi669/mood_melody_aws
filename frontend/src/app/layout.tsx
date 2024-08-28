36c97ed2bc79ac8b1c057b3cdea7c4172d02ac71
cmjiwa <wangjinghuan@yahoo.com>
2024-08-28 15:02:15 +0300

    Add logo to browser tab

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -24,6 +24,39 @@ export default function RootLayout({
         <meta charSet="UTF-8" />
         <title>{metadata.title}</title>
         <meta name="description" content={metadata.description} />
+
+        {/* linke is commonly used in the 'head' section of a webpage to link to stylesheets, favicons, <Link> component is used for client-side navigation between pages in a Next.js application*/}
+        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="192x192"
+          href="/android-chrome-192x192.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="512x512"
+          href="/android-chrome-512x512.png"
+        />
       </Head>
       <body>
         <MediaProvider>
