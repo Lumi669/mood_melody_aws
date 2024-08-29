328870992689ea2ca5825b1bff14c33cc757d485
cmjiwa <wangjinghuan@yahoo.com>
2024-08-29 11:23:27 +0300

    Add dancing-girl-removebg.png to home page and add potential useful .mp4 to public/"

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Image from "next/image";
 
 import TestForm from "../../components/TestForm";
 import { fetchAllMusicWithImages } from "utils/fetchAllMusicWithImages";
@@ -10,9 +11,28 @@ const Home: React.FC = async () => {
 
   return (
     <div>
-      <h1 className="m-20 p-20 bg-yellow-100 text-4xl font-mono text-green-500 font-bold tracking-wide text-center">
-        Hi there, How do you feel today :D ?
-      </h1>
+      <div className="flex justify-center">
+        {/* <video
+          src="/animation-center-yellowbg-noblinking.mp4"
+          autoPlay
+          loop
+          muted
+          className="w-1/4 h-auto" // Adjust size and classes as needed
+        /> */}
+
+        {/* Use the Next.js Image component for optimized images */}
+        <Image
+          src="/dancing-girl-removebg.png"
+          alt="Description of the image"
+          width={300}
+          height={300}
+          className="rounded-lg"
+        />
+
+        <h1 className="m-10 p-20 bg-yellow-100 text-4xl font-mono text-green-500 font-bold tracking-wide text-center">
+          Hi there, How do you feel today :D ?
+        </h1>
+      </div>
       <ClientWrapperForMusicPlayer />
       <ClientContextUpdater initialData={matchedData} />
     </div>
