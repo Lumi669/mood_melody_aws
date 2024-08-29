03a3252335cb1f626ca3b54a84b0413b064c420c
cmjiwa <wangjinghuan@yahoo.com>
2024-08-29 16:02:06 +0300

    Delete progress bar for music started by clicking happy or sad button

--- frontend/components/MusicPlayer.tsx
+++ frontend/components/MusicPlayer.tsx
@@ -19,9 +19,6 @@ const MusicPlayer: React.FC = () => {
   } = useMedia();
   const [currentImageUrl, setCurrentImageUrl] = useState<string | null>(null);
   const [currentMusicName, setCurrentMusicName] = useState<string | null>(null);
-  const [currentTime, setCurrentTime] = useState<number>(0); // For progress bar
-  const [duration, setDuration] = useState<number>(0); // For progress bar
-  const [isDragging, setIsDragging] = useState<boolean>(false); // For draggable progress bar
 
   const playMusic = (mood: "happy" | "sad") => {
     stopMusic(); // Stop any currently playing music
@@ -45,41 +42,6 @@ const MusicPlayer: React.FC = () => {
       togglePlayPause(currentTrack); // Toggle play/pause using the context function
     }
   };
-
-  const handleSliderChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const time = parseFloat(event.target.value);
-    if (currentTrack && isPlaying) {
-      const newAudio = new Audio(currentTrack);
-      newAudio.currentTime = time;
-      setCurrentTime(time);
-    }
-  };
-
-  const handleSliderStart = () => {
-    setIsDragging(true);
-  };
-
-  const handleSliderEnd = () => {
-    setIsDragging(false);
-  };
-
-  useEffect(() => {
-    if (currentTrack) {
-      const newAudio = new Audio(currentTrack);
-      newAudio.addEventListener("loadedmetadata", () =>
-        setDuration(newAudio.duration),
-      );
-      newAudio.addEventListener("timeupdate", () => {
-        if (!isDragging) {
-          setCurrentTime(newAudio.currentTime);
-        }
-      });
-      return () => {
-        newAudio.pause();
-        newAudio.currentTime = 0;
-      };
-    }
-  }, [currentTrack, isDragging]);
 
   return (
     <>
@@ -108,21 +70,6 @@ const MusicPlayer: React.FC = () => {
             height={800}
             // onClick={handleImageClick} // This now properly toggles play/pause
             className="cursor-pointer"
-          />
-        )}
-        {currentTrack && (
-          <input
-            type="range"
-            min="0"
-            max={duration || 1} // Ensure max is never 0 to avoid a React error
-            value={currentTime}
-            onChange={handleSliderChange}
-            onMouseDown={handleSliderStart}
-            onMouseUp={handleSliderEnd}
-            onTouchStart={handleSliderStart}
-            onTouchEnd={handleSliderEnd}
-            style={{ width: "100%" }}
-            className="absolute bottom-0 left-0 w-full"
           />
         )}
       </div>
