1c584217cc80c645e3274f0f7c61fa71ec9fc055
cmjiwa <wangjinghuan@yahoo.com>
2024-08-30 23:31:56 +0300

    Style live page, display nomusic-playing-bird.mp4 if no playing music, center image and music info if there is playing music

--- frontend/src/app/live/page.tsx
+++ frontend/src/app/live/page.tsx
@@ -1,63 +1,3 @@
-// "use client";
-
-// import React, { useState, useEffect } from "react";
-// import { useMedia } from "../../../context/MediaContext";
-// import CustomImage from "components/CustomImage";
-// import { extractAuthorName } from "../../../utils/extractMusicInfo";
-// import { extractMusicName } from "../../../utils/extractMusicInfo";
-
-// const Liveplay = () => {
-//   const [hasImage, setHasImage] = useState(false);
-//   const { currentTrack, currentSong } = useMedia();
-//   console.log("current song from live.tsx === ", currentSong);
-//   console.log("current track from live.tsx === ", currentTrack);
-
-//   let authorName;
-//   let musicName;
-
-//   // Use useEffect to update hasImage when currentSong changes
-//   useEffect(() => {
-//     if (currentSong && currentSong.name) {
-//       setHasImage(true);
-//     } else {
-//       setHasImage(false);
-//     }
-//   }, [currentSong]);
-
-//   if (currentSong && currentSong.name) {
-//     authorName = extractAuthorName(currentSong.name);
-//     musicName = extractMusicName(currentSong.name);
-//   } else {
-//     authorName = "No author name specified";
-//     musicName = "Oh oh ... No music is playing";
-//   }
-
-//   return (
-//     <div className="flex items-center p-4">
-//       {currentSong && (
-//         <div className="w-1/2">
-//           <CustomImage
-//             src={currentSong.imgUrl}
-//             alt={currentSong.name}
-//             dataUrl={currentTrack}
-//             layout="responsive"
-//             width={800}
-//             height={800}
-//             className="cursor-pointer"
-//           />
-//         </div>
-//       )}
-//       <div className="w-1/2 pl-4 ">
-//         <h1 className="text-xl font-bold ">Music Name</h1>
-//         <div>{musicName}</div>
-//         <h2 className="text-xl font-bold">Author Name</h2>
-//         <div>{authorName}</div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Liveplay;
 "use client";
 
 import React, { useState, useEffect } from "react";
@@ -93,9 +33,7 @@ const Liveplay = () => {
   }
 
   return (
-    <div
-      className={`flex items-center p-4 ${hasImage ? "" : "justify-center text-center"}`}
-    >
+    <div className="flex items-center p-4 justify-center text-center">
       {currentSong && (
         <div className="w-1/2">
           <CustomImage
@@ -105,16 +43,29 @@ const Liveplay = () => {
             layout="responsive"
             width={800}
             height={800}
-            className="cursor-pointer"
+            className="cursor-pointer m-10"
+          />
+          <div className="pl-4">
+            <h1 className="text-xl font-bold">Music Name</h1>
+            <div className="pb-8">{musicName}</div>
+            <h2 className="text-xl font-bold">Author Name</h2>
+            <div>{authorName}</div>
+          </div>
+        </div>
+      )}
+
+      {/* Video in the bottom right corner */}
+      {!hasImage && (
+        <div className="fixed inset-0 flex justify-center items-center overflow-hidden">
+          <video
+            src="/nomusic-playing-bird.mp4"
+            autoPlay
+            loop
+            muted
+            className="w-3/4 h-2/3"
           />
         </div>
       )}
-      <div className={`${hasImage ? "w-1/2 pl-4" : "w-full"}`}>
-        <h1 className="text-xl font-bold">Music Name</h1>
-        <div>{musicName}</div>
-        <h2 className="text-xl font-bold">Author Name</h2>
-        <div>{authorName}</div>
-      </div>
     </div>
   );
 };
