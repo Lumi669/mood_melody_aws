a7067527391e5873f539956037e0c534efd2f8b3
cmjiwa <wangjinghuan@yahoo.com>
2024-09-07 00:20:56 +0300

    Now when navigate back to homepage, homepage show its last played music for corresonding current mood even though next or previous button has been clicked on allmusic or live or playlist page

--- frontend/components/GlobalControls.tsx
+++ frontend/components/GlobalControls.tsx
@@ -1,3 +1,160 @@
+// "use client";
+
+// import React, { useState, useEffect } from "react";
+// import { useMedia } from "@context/MediaContext";
+// import { MusicWithImageSimplified } from "../types/type";
+// import { usePathname } from "next/navigation"; // Import usePathname from next/navigation
+
+// const GlobalControls: React.FC = () => {
+//   const { stopMusic, togglePlayPause, isPlaying, currentSong, skipTrack } =
+//     useMedia();
+//   const pathname = usePathname(); // Initialize usePathname
+//   const [animate, setAnimate] = useState(false);
+
+//   useEffect(() => {
+//     if (isPlaying) {
+//       setAnimate(true);
+//     }
+//   }, [isPlaying, currentSong]);
+
+//   const handlePlayPause = () => {
+//     togglePlayPause();
+//   };
+
+//   const updatePlaylist = (newPlaylist: MusicWithImageSimplified[]) => {
+//     localStorage.setItem("playlist", JSON.stringify(newPlaylist));
+//     window.dispatchEvent(new Event("playlistUpdated"));
+//   };
+
+//   const handleSkipTrack = (direction: "next" | "previous") => {
+//     // Use pathname to check if the current page is the homepage
+//     const isHomePage = pathname === "/"; // Adjust the path to match your homepage
+//     const updatedPlaylist = skipTrack(direction, isHomePage); // Pass both direction and isHomePage
+
+//     updatePlaylist(updatedPlaylist);
+//   };
+
+//   return (
+//     <div
+//       className={`fixed bottom-0 left-0 w-full p-4 bg-gray-800 text-white flex flex-col items-center justify-center z-50 transition-all duration-500 ${
+//         animate ? "animate-fly-in" : ""
+//       }`}
+//     >
+//       {currentSong && (
+//         <div className="flex items-center space-x-4">
+//           <button
+//             onClick={handlePlayPause}
+//             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-24 text-center"
+//           >
+//             {isPlaying ? "Pause" : "Play "}
+//           </button>
+//           <button
+//             onClick={stopMusic}
+//             className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+//           >
+//             Stop Music
+//           </button>
+//           <button
+//             onClick={() => handleSkipTrack("previous")}
+//             className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded"
+//           >
+//             Previous
+//           </button>
+//           <button
+//             onClick={() => handleSkipTrack("next")}
+//             className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded"
+//           >
+//             Next
+//           </button>
+//         </div>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default GlobalControls;
+
+// "use client";
+
+// import React, { useState, useEffect } from "react";
+// import { useMedia } from "@context/MediaContext";
+// import { MusicWithImageSimplified } from "../types/type";
+// import { usePathname } from "next/navigation"; // Import usePathname from next/navigation
+
+// const GlobalControls: React.FC = () => {
+//   const { stopMusic, togglePlayPause, isPlaying, currentSong, skipTrack } =
+//     useMedia();
+//   const pathname = usePathname(); // Initialize usePathname
+//   const [animate, setAnimate] = useState(false);
+
+//   useEffect(() => {
+//     if (isPlaying) {
+//       setAnimate(true);
+//     }
+//   }, [isPlaying, currentSong]);
+
+//   const handlePlayPause = () => {
+//     togglePlayPause();
+//   };
+
+//   const updatePlaylist = (newPlaylist: MusicWithImageSimplified[]) => {
+//     localStorage.setItem("playlist", JSON.stringify(newPlaylist));
+//     window.dispatchEvent(new Event("playlistUpdated"));
+//   };
+
+//   const handleSkipTrack = (direction: "next" | "previous") => {
+//     const isHomePage = pathname === "/"; // Check if the current page is the homepage
+//     const updatedPlaylist = skipTrack(direction, isHomePage); // Pass both direction and isHomePage
+
+//     // Save the current song in localStorage if on the homepage
+//     if (isHomePage && currentSong) {
+//       localStorage.setItem(
+//         `lastPlayed_${currentSong.mood}`,
+//         JSON.stringify(currentSong),
+//       );
+//     }
+
+//     updatePlaylist(updatedPlaylist);
+//   };
+
+//   return (
+//     <div
+//       className={`fixed bottom-0 left-0 w-full p-4 bg-gray-800 text-white flex flex-col items-center justify-center z-50 transition-all duration-500 ${animate ? "animate-fly-in" : ""}`}
+//     >
+//       {currentSong && (
+//         <div className="flex items-center space-x-4">
+//           <button
+//             onClick={handlePlayPause}
+//             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-24 text-center"
+//           >
+//             {isPlaying ? "Pause" : "Play "}
+//           </button>
+//           <button
+//             onClick={stopMusic}
+//             className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+//           >
+//             Stop Music
+//           </button>
+//           <button
+//             onClick={() => handleSkipTrack("previous")}
+//             className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded"
+//           >
+//             Previous
+//           </button>
+//           <button
+//             onClick={() => handleSkipTrack("next")}
+//             className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded"
+//           >
+//             Next
+//           </button>
+//         </div>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default GlobalControls;
+
 "use client";
 
 import React, { useState, useEffect } from "react";
@@ -17,6 +174,19 @@ const GlobalControls: React.FC = () => {
     }
   }, [isPlaying, currentSong]);
 
+  // Use useEffect to update localStorage whenever currentSong changes and is on the homepage
+  useEffect(() => {
+    const isHomePage = pathname === "/"; // Check if the current page is the homepage
+
+    if (isHomePage && currentSong) {
+      localStorage.setItem("currentMood", currentSong.mood);
+      localStorage.setItem(
+        `lastPlayed_${currentSong.mood}`,
+        JSON.stringify(currentSong),
+      );
+    }
+  }, [currentSong, pathname]);
+
   const handlePlayPause = () => {
     togglePlayPause();
   };
@@ -27,8 +197,7 @@ const GlobalControls: React.FC = () => {
   };
 
   const handleSkipTrack = (direction: "next" | "previous") => {
-    // Use pathname to check if the current page is the homepage
-    const isHomePage = pathname === "/"; // Adjust the path to match your homepage
+    const isHomePage = pathname === "/"; // Check if the current page is the homepage
     const updatedPlaylist = skipTrack(direction, isHomePage); // Pass both direction and isHomePage
 
     updatePlaylist(updatedPlaylist);
@@ -36,9 +205,7 @@ const GlobalControls: React.FC = () => {
 
   return (
     <div
-      className={`fixed bottom-0 left-0 w-full p-4 bg-gray-800 text-white flex flex-col items-center justify-center z-50 transition-all duration-500 ${
-        animate ? "animate-fly-in" : ""
-      }`}
+      className={`fixed bottom-0 left-0 w-full p-4 bg-gray-800 text-white flex flex-col items-center justify-center z-50 transition-all duration-500 ${animate ? "animate-fly-in" : ""}`}
     >
       {currentSong && (
         <div className="flex items-center space-x-4">
--- frontend/components/MusicPlayer.tsx
+++ frontend/components/MusicPlayer.tsx
@@ -1,3 +1,438 @@
+// "use client";
+
+// import React, { useState, useEffect, useCallback } from "react";
+// import { useMedia } from "@context/MediaContext";
+// import { addToPlaylist22 } from "@utils/addToPlaylist";
+// import CustomImage from "@components/CustomImage";
+// import SentimentAnalysisPage from "@components/Sentimentanalysis";
+// import Greeting from "./Greeting";
+// import Image from "next/image";
+
+// const MusicPlayer: React.FC = () => {
+//   console.log("MusicPlayer   ggggg get called ...");
+//   const {
+//     mediaData,
+//     setIsRed,
+//     setIsBlue,
+//     setIsBrown,
+//     playTrack,
+//     stopMusic,
+//     setCurrentSong,
+//     currentTrack,
+//     currentSong,
+//     isPlaying,
+//     isRed,
+//     isBlue,
+//     isBrown,
+//   } = useMedia();
+//   const [currentMusicName, setCurrentMusicName] = useState<string | null>(null);
+//   const [isOriginalViewVisible, setOriginalViewVisible] = useState(true);
+//   const [isAnimationActive, setAnimationActive] = useState(false);
+//   const [isVideoVisible, setVideoVisible] = useState(false);
+//   const [currentMusicCtg, setCurrentMusicCtg] = useState<string | null>(null);
+//   const [moodSource, setMoodSource] = useState<"button" | "analysis" | null>(
+//     null,
+//   );
+//   const [sentimentMessage, setSentimentMessage] = useState<string>("");
+
+//   console.log("=======1111111");
+
+//   useEffect(() => {
+//     console.log("=======2222222");
+//     // Retrieve state from localStorage when the component mounts
+//     const storedMoodSource = localStorage.getItem("moodSource");
+//     const storedSentimentMessage = localStorage.getItem("sentimentMessage");
+
+//     if (storedMoodSource) {
+//       console.log("=======3333333");
+
+//       setMoodSource(storedMoodSource as "button" | "analysis");
+//     }
+
+//     if (storedSentimentMessage) {
+//       console.log("=======44444444");
+
+//       console.log("ssss storedSentimentMessage === ", storedSentimentMessage);
+//       setSentimentMessage(storedSentimentMessage);
+//     }
+//   }, []);
+
+//   console.log("=======??????");
+
+//   const playMusic = (
+//     mood: "happy" | "sad" | "calm",
+//     source: "button" | "analysis",
+//   ) => {
+//     stopMusic(); // Stop any currently playing music
+//     setMoodSource(source);
+
+//     const filteredSongs = mediaData.filter((song) => song.mood === mood);
+//     const randomSong =
+//       filteredSongs[Math.floor(Math.random() * filteredSongs.length)];
+//     setCurrentSong(randomSong);
+//     addToPlaylist22(randomSong);
+
+//     console.log("random song === ", randomSong);
+
+//     setIsRed(randomSong.mood === "happy");
+//     setIsBlue(randomSong.mood === "sad");
+//     setIsBrown(randomSong.mood === "calm");
+
+//     setCurrentMusicName(randomSong.name);
+//     setCurrentMusicCtg(randomSong.ctg);
+
+//     playTrack(randomSong.url); // Play the new track using the global context
+
+//     // Trigger the animation sequence
+//     setOriginalViewVisible(false);
+//     setAnimationActive(true);
+
+//     setTimeout(() => {
+//       setAnimationActive(false);
+//       setVideoVisible(true);
+//     }, 1000); // Adjust to match the animation duration
+//   };
+
+//   useEffect(() => {
+//     // Check if music is playing when the component mounts
+//     if (currentSong) {
+//       setOriginalViewVisible(false);
+//       setVideoVisible(true);
+//     } else {
+//       setOriginalViewVisible(true); // Ensure the original view is visible when there's no current song
+//       setVideoVisible(false);
+//     }
+//   }, [currentSong]);
+
+//   const handleVideoClick = () => {
+//     setOriginalViewVisible(true); // Show the original view
+//     setAnimationActive(false); // Stop any ongoing animation
+//     setVideoVisible(false); // Hide the video
+//     stopMusic(); // Stop the music when returning to the original view
+//   };
+
+//   const OrginalViewPart = () => {
+//     return (
+//       <>
+//         <div>
+//           <Greeting />
+//         </div>
+//         <div>
+//           <SentimentAnalysisPage
+//             onSentimentAnalyzed={handleSentimentAnalysis}
+//             playMusic={playMusic} // Pass playMusic as a prop
+//           />
+//         </div>
+//       </>
+//     );
+//   };
+
+//   const handleSentimentAnalysis = useCallback((message: string) => {
+//     console.log("hanleSentimentAnalysis get called ...");
+//     console.log("mmmmmmm essage from hanleSentimentAnalysis === ", message); //new / updated message
+//     console.log(
+//       "sentiment message from handleSentimentAnalysis === ",
+//       sentimentMessage, //old message
+//     );
+
+//     setSentimentMessage(message);
+//     setMoodSource("analysis");
+
+//     localStorage.setItem("moodSource", "analysis");
+//     console.log(
+//       "bbbbbb    sentiment message from handleSentimentAnalysis === ",
+//       sentimentMessage, //old message
+//     );
+//     localStorage.setItem("sentimentMessage", message);
+//   }, []);
+
+//   const getMoodMessage = () => {
+//     if (moodSource === "analysis") {
+//       return sentimentMessage;
+//     } else if (moodSource === "button") {
+//       if (isRed) return "You seem happy";
+//       if (isBlue) return "You seem sad";
+//       if (isBrown) return "You seem peaceful";
+//     }
+
+//     return "You have no emotion at the moment";
+//   };
+
+//   console.log("getMoodMessage() ===== ", getMoodMessage());
+
+//   return (
+//     <>
+//       <div className="relative min-h-screen px-4">
+//         {/* Original Homepage View */}
+//         {isOriginalViewVisible && <OrginalViewPart />}
+
+//         {/* Buttons for mood selection */}
+//         <button onClick={() => playMusic("happy", "button")} className="m-5">
+//           Happy
+//         </button>
+//         <button onClick={() => playMusic("sad", "button")} className="m-5">
+//           Sad
+//         </button>
+//         <button onClick={() => playMusic("calm", "button")} className="m-5">
+//           Calm
+//         </button>
+
+//         {/* Animation from image to video */}
+//         {isAnimationActive && (
+//           <Image
+//             src="/dancing-girl-removebg.webp"
+//             alt="Animation"
+//             width={100}
+//             height={100}
+//             className="fixed transition-all duration-1000 transform animate-fly-to-corner"
+//           />
+//         )}
+
+//         {/* Video in the bottom right corner */}
+//         {isVideoVisible && (
+//           <div className="video-on-top" onClick={handleVideoClick}>
+//             <video
+//               src="/animation-center-yellowbg-noblinking.mp4"
+//               autoPlay
+//               loop
+//               muted
+//               className="w-full h-full"
+//             />
+//             {/* Display mood message */}
+//             <div className="absolute top-0 left-0 right-0 text-center p-4 bg-opacity-75 bg-black text-white font-bold">
+//               {getMoodMessage()}
+//             </div>
+//           </div>
+//         )}
+
+//         {/* Display the current track image */}
+//         {currentSong && currentSong.imgUrl && (
+//           <div className="flex items-center justify-center h-full">
+//             <CustomImage
+//               src={currentSong.imgUrl}
+//               alt={currentMusicName || "an image associated with the music"}
+//               dataUrl={currentTrack}
+//               layout="responsive"
+//               width={1400}
+//               height={700}
+//               className="cursor-pointer w-full max-w-full lg:max-w-[1400px]"
+//               ctg={currentMusicCtg}
+//               mood={currentSong.mood}
+//             />
+//           </div>
+//         )}
+//       </div>
+//     </>
+//   );
+// };
+
+// export default MusicPlayer;
+
+// "use client";
+
+// import React, { useState, useEffect, useCallback } from "react";
+// import { useMedia } from "@context/MediaContext";
+// import { addToPlaylist22 } from "@utils/addToPlaylist";
+// import CustomImage from "@components/CustomImage";
+// import SentimentAnalysisPage from "@components/Sentimentanalysis";
+// import Greeting from "./Greeting";
+// import Image from "next/image";
+
+// const MusicPlayer: React.FC = () => {
+//   const {
+//     mediaData,
+//     setIsRed,
+//     setIsBlue,
+//     setIsBrown,
+//     playTrack,
+//     stopMusic,
+//     setCurrentSong,
+//     currentTrack,
+//     currentSong,
+//     isPlaying,
+//     isRed,
+//     isBlue,
+//     isBrown,
+//   } = useMedia();
+
+//   const [currentMusicName, setCurrentMusicName] = useState<string | null>(null);
+//   const [isOriginalViewVisible, setOriginalViewVisible] = useState(true);
+//   const [isAnimationActive, setAnimationActive] = useState(false);
+//   const [isVideoVisible, setVideoVisible] = useState(false);
+//   const [currentMusicCtg, setCurrentMusicCtg] = useState<string | null>(null);
+//   const [moodSource, setMoodSource] = useState<"button" | "analysis" | null>(
+//     null,
+//   );
+//   const [sentimentMessage, setSentimentMessage] = useState<string>("");
+
+//   useEffect(() => {
+//     // Retrieve the last played song for each mood from localStorage when the component mounts
+//     const mood = localStorage.getItem("currentMood");
+//     if (mood) {
+//       const lastPlayedSong = localStorage.getItem(`lastPlayed_${mood}`);
+//       if (lastPlayedSong) {
+//         const song = JSON.parse(lastPlayedSong);
+//         setCurrentSong(song); // Set the last played song
+//         playTrack(song.url, song); // Play the last played song
+//       }
+//     }
+//   }, []);
+
+//   const playMusic = (
+//     mood: "happy" | "sad" | "calm",
+//     source: "button" | "analysis",
+//   ) => {
+//     stopMusic(); // Stop any currently playing music
+//     setMoodSource(source);
+
+//     // Store the current mood in localStorage
+//     localStorage.setItem("currentMood", mood);
+
+//     const filteredSongs = mediaData.filter((song) => song.mood === mood);
+//     const randomSong =
+//       filteredSongs[Math.floor(Math.random() * filteredSongs.length)];
+//     setCurrentSong(randomSong);
+//     addToPlaylist22(randomSong);
+
+//     setIsRed(randomSong.mood === "happy");
+//     setIsBlue(randomSong.mood === "sad");
+//     setIsBrown(randomSong.mood === "calm");
+
+//     setCurrentMusicName(randomSong.name);
+//     setCurrentMusicCtg(randomSong.ctg);
+
+//     playTrack(randomSong.url); // Play the new track using the global context
+
+//     setOriginalViewVisible(false);
+//     setAnimationActive(true);
+
+//     setTimeout(() => {
+//       setAnimationActive(false);
+//       setVideoVisible(true);
+//     }, 1000); // Adjust to match the animation duration
+//   };
+
+//   useEffect(() => {
+//     if (currentSong) {
+//       setOriginalViewVisible(false);
+//       setVideoVisible(true);
+//     } else {
+//       setOriginalViewVisible(true); // Ensure the original view is visible when there's no current song
+//       setVideoVisible(false);
+//     }
+//   }, [currentSong]);
+
+//   const handleVideoClick = () => {
+//     setOriginalViewVisible(true); // Show the original view
+//     setAnimationActive(false); // Stop any ongoing animation
+//     setVideoVisible(false); // Hide the video
+//     stopMusic(); // Stop the music when returning to the original view
+//   };
+
+//   const OriginalViewPart = () => {
+//     return (
+//       <>
+//         <div>
+//           <Greeting />
+//         </div>
+//         <div>
+//           <SentimentAnalysisPage
+//             onSentimentAnalyzed={handleSentimentAnalysis}
+//             playMusic={playMusic} // Pass playMusic as a prop
+//           />
+//         </div>
+//       </>
+//     );
+//   };
+
+//   const handleSentimentAnalysis = useCallback((message: string) => {
+//     setSentimentMessage(message);
+//     setMoodSource("analysis");
+
+//     localStorage.setItem("moodSource", "analysis");
+//     localStorage.setItem("sentimentMessage", message);
+//   }, []);
+
+//   const getMoodMessage = () => {
+//     if (moodSource === "analysis") {
+//       return sentimentMessage;
+//     } else if (moodSource === "button") {
+//       if (isRed) return "You seem happy";
+//       if (isBlue) return "You seem sad";
+//       if (isBrown) return "You seem peaceful";
+//     }
+
+//     return "You have no emotion at the moment";
+//   };
+
+//   return (
+//     <>
+//       <div className="relative min-h-screen px-4">
+//         {/* Original Homepage View */}
+//         {isOriginalViewVisible && <OriginalViewPart />}
+
+//         {/* Buttons for mood selection */}
+//         <button onClick={() => playMusic("happy", "button")} className="m-5">
+//           Happy
+//         </button>
+//         <button onClick={() => playMusic("sad", "button")} className="m-5">
+//           Sad
+//         </button>
+//         <button onClick={() => playMusic("calm", "button")} className="m-5">
+//           Calm
+//         </button>
+
+//         {/* Animation from image to video */}
+//         {isAnimationActive && (
+//           <Image
+//             src="/dancing-girl-removebg.webp"
+//             alt="Animation"
+//             width={100}
+//             height={100}
+//             className="fixed transition-all duration-1000 transform animate-fly-to-corner"
+//           />
+//         )}
+
+//         {/* Video in the bottom right corner */}
+//         {isVideoVisible && (
+//           <div className="video-on-top" onClick={handleVideoClick}>
+//             <video
+//               src="/animation-center-yellowbg-noblinking.mp4"
+//               autoPlay
+//               loop
+//               muted
+//               className="w-full h-full"
+//             />
+//             {/* Display mood message */}
+//             <div className="absolute top-0 left-0 right-0 text-center p-4 bg-opacity-75 bg-black text-white font-bold">
+//               {getMoodMessage()}
+//             </div>
+//           </div>
+//         )}
+
+//         {/* Display the current track image */}
+//         {currentSong && currentSong.imgUrl && (
+//           <div className="flex items-center justify-center h-full">
+//             <CustomImage
+//               src={currentSong.imgUrl}
+//               alt={currentMusicName || "an image associated with the music"}
+//               dataUrl={currentTrack}
+//               layout="responsive"
+//               width={1400}
+//               height={700}
+//               className="cursor-pointer w-full max-w-full lg:max-w-[1400px]"
+//               ctg={currentMusicCtg}
+//               mood={currentSong.mood}
+//             />
+//           </div>
+//         )}
+//       </div>
+//     </>
+//   );
+// };
+
+// export default MusicPlayer;
+
 "use client";
 
 import React, { useState, useEffect, useCallback } from "react";
@@ -9,7 +444,6 @@ import Greeting from "./Greeting";
 import Image from "next/image";
 
 const MusicPlayer: React.FC = () => {
-  console.log("MusicPlayer   ggggg get called ...");
   const {
     mediaData,
     setIsRed,
@@ -21,10 +455,12 @@ const MusicPlayer: React.FC = () => {
     currentTrack,
     currentSong,
     isPlaying,
+    setIsPlaying,
     isRed,
     isBlue,
     isBrown,
   } = useMedia();
+
   const [currentMusicName, setCurrentMusicName] = useState<string | null>(null);
   const [isOriginalViewVisible, setOriginalViewVisible] = useState(true);
   const [isAnimationActive, setAnimationActive] = useState(false);
@@ -35,29 +471,21 @@ const MusicPlayer: React.FC = () => {
   );
   const [sentimentMessage, setSentimentMessage] = useState<string>("");
 
-  console.log("=======1111111");
-
+  // Retrieve the last played song when the component mounts and force playback
   useEffect(() => {
-    console.log("=======2222222");
-    // Retrieve state from localStorage when the component mounts
-    const storedMoodSource = localStorage.getItem("moodSource");
-    const storedSentimentMessage = localStorage.getItem("sentimentMessage");
-
-    if (storedMoodSource) {
-      console.log("=======3333333");
-
-      setMoodSource(storedMoodSource as "button" | "analysis");
-    }
-
-    if (storedSentimentMessage) {
-      console.log("=======44444444");
-
-      console.log("ssss storedSentimentMessage === ", storedSentimentMessage);
-      setSentimentMessage(storedSentimentMessage);
+    const currentMood = localStorage.getItem("currentMood");
+    if (currentMood) {
+      const lastPlayedSong = localStorage.getItem(`lastPlayed_${currentMood}`);
+      if (lastPlayedSong) {
+        const song = JSON.parse(lastPlayedSong);
+        setCurrentSong(song); // Set the last played song
+        setTimeout(() => {
+          playTrack(song.url, song); // Play the last played song immediately
+          setIsPlaying(true); // Force set the state to playing
+        }, 0);
+      }
     }
   }, []);
-
-  console.log("=======??????");
 
   const playMusic = (
     mood: "happy" | "sad" | "calm",
@@ -66,13 +494,14 @@ const MusicPlayer: React.FC = () => {
     stopMusic(); // Stop any currently playing music
     setMoodSource(source);
 
+    // Store the current mood in localStorage
+    localStorage.setItem("currentMood", mood);
+
     const filteredSongs = mediaData.filter((song) => song.mood === mood);
     const randomSong =
       filteredSongs[Math.floor(Math.random() * filteredSongs.length)];
     setCurrentSong(randomSong);
     addToPlaylist22(randomSong);
-
-    console.log("random song === ", randomSong);
 
     setIsRed(randomSong.mood === "happy");
     setIsBlue(randomSong.mood === "sad");
@@ -83,7 +512,6 @@ const MusicPlayer: React.FC = () => {
 
     playTrack(randomSong.url); // Play the new track using the global context
 
-    // Trigger the animation sequence
     setOriginalViewVisible(false);
     setAnimationActive(true);
 
@@ -94,7 +522,6 @@ const MusicPlayer: React.FC = () => {
   };
 
   useEffect(() => {
-    // Check if music is playing when the component mounts
     if (currentSong) {
       setOriginalViewVisible(false);
       setVideoVisible(true);
@@ -111,7 +538,7 @@ const MusicPlayer: React.FC = () => {
     stopMusic(); // Stop the music when returning to the original view
   };
 
-  const OrginalViewPart = () => {
+  const OriginalViewPart = () => {
     return (
       <>
         <div>
@@ -128,21 +555,10 @@ const MusicPlayer: React.FC = () => {
   };
 
   const handleSentimentAnalysis = useCallback((message: string) => {
-    console.log("hanleSentimentAnalysis get called ...");
-    console.log("mmmmmmm essage from hanleSentimentAnalysis === ", message); //new / updated message
-    console.log(
-      "sentiment message from handleSentimentAnalysis === ",
-      sentimentMessage, //old message
-    );
-
     setSentimentMessage(message);
     setMoodSource("analysis");
 
     localStorage.setItem("moodSource", "analysis");
-    console.log(
-      "bbbbbb    sentiment message from handleSentimentAnalysis === ",
-      sentimentMessage, //old message
-    );
     localStorage.setItem("sentimentMessage", message);
   }, []);
 
@@ -158,13 +574,11 @@ const MusicPlayer: React.FC = () => {
     return "You have no emotion at the moment";
   };
 
-  console.log("getMoodMessage() ===== ", getMoodMessage());
-
   return (
     <>
       <div className="relative min-h-screen px-4">
         {/* Original Homepage View */}
-        {isOriginalViewVisible && <OrginalViewPart />}
+        {isOriginalViewVisible && <OriginalViewPart />}
 
         {/* Buttons for mood selection */}
         <button onClick={() => playMusic("happy", "button")} className="m-5">
