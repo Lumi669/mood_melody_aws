52a7617ca0d70b9e79e429dcde30fde32465733a
cmjiwa <wangjinghuan@yahoo.com>
2024-09-09 17:38:40 +0300

    Make privacy page scrollable

--- frontend/src/app/About/privacy/page.tsx
+++ frontend/src/app/About/privacy/page.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Privacy = () => {
   return (
-    <div className="container mx-auto p-4">
+    <div className="h-screen overflow-y-scroll p-4 pb-40">
       <h1 className="text-2xl font-bold mb-4">Privacy Policy</h1>
       <p className="text-gray-700 mb-4">
         This Privacy Policy explains how this app handles your data. Please note
