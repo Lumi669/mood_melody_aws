819d36ece6bc24c8f33e4f891c36fc0380c18b54
cmjiwa <wangjinghuan@yahoo.com>
2024-09-09 13:55:22 +0300

    Escape ' to fix the frontend github actions Build docker image error

--- frontend/src/app/About/page.tsx
+++ frontend/src/app/About/page.tsx
@@ -5,8 +5,8 @@ const About = () => {
     <div>
       <h1 className="text-center mt-10 text-6xl">About This App</h1>
       <p className="indent-8 m-20 text-xl">
-        Welcome to my About page. Here, you'll find information about the app.
-        Explore corresponding sections for more details on the technology,
+        Welcome to my About page. Here, you&apos;ll find information about the
+        app. Explore corresponding sections for more details on the technology,
         privacy policy, and testimonials and analysis.
       </p>
     </div>
