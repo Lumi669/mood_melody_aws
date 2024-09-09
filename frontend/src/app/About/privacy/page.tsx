738ddff8872d2f81efd9915857af3b8f7756021b
cmjiwa <wangjinghuan@yahoo.com>
2024-09-09 17:06:18 +0300

    Implement privacy page

---
+++ frontend/src/app/About/privacy/page.tsx
@@ -0,0 +1,201 @@
+import React from "react";
+
+const Privacy = () => {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-2xl font-bold mb-4">Privacy Policy</h1>
+      <p className="text-gray-700 mb-4">
+        This Privacy Policy explains how this app handles your data. Please note
+        that this app is a demonstration project created to showcase my
+        development skills, and it is not intended for commercial use.
+        Therefore, it does not actively collect or store personal data.
+      </p>
+
+      <h2 className="text-xl font-semibold mb-2">
+        Content Usage and Attribution
+      </h2>
+      <p className="text-gray-700 mb-4">
+        This app includes animations and music sourced from online platforms
+        under their respective licenses:
+      </p>
+      <ul className="list-disc pl-6 mb-4">
+        <li>
+          Animations from{" "}
+          <a
+            href="https://www.veed.io/"
+            className="text-blue-500 hover:underline"
+          >
+            Veed.io
+          </a>
+          , used according to their licensing terms.
+        </li>
+        <li>
+          Music from{" "}
+          <a
+            href="https://pixabay.com/"
+            className="text-blue-500 hover:underline"
+          >
+            Pixabay
+          </a>
+          , licensed for free use.
+        </li>
+      </ul>
+
+      <p className="text-gray-700 mb-4">
+        This app includes images sourced from{" "}
+        <a
+          href="https://www.freepik.com"
+          className="text-blue-500 hover:underline"
+        >
+          Freepik
+        </a>
+        . These images are used under Freepik's free license with proper
+        attribution to the original authors:
+      </p>
+      <ul className="list-disc pl-6 mb-4">
+        <li>
+          <a
+            href="https://www.freepik.com/free-photo/adorable-little-poodle-with-cute-bunny-ears-beige-surface_15672270.htm"
+            className="text-blue-500 hover:underline"
+          >
+            Image by wirestock on Freepik
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.freepik.com/free-photo/happy-beagle-with-droopy-ears-blue-background_135010608.htm"
+            className="text-blue-500 hover:underline"
+          >
+            Image by rorozoa on Freepik
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.freepik.com/free-photo/medium-shot-friends-eating-pizza_138239739.htm"
+            className="text-blue-500 hover:underline"
+          >
+            Image by Drazen Zigic on Freepik
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.freepik.com/free-photo/cute-puppy-sitting-grass-enjoying-nature-playful-beauty-generated-by-artificial-intelligence_79687547.htm"
+            className="text-blue-500 hover:underline"
+          >
+            Image by upklyak on Freepik
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.freepik.com/free-photo/close-up-adorable-dog-laying-blanket_10422541.htm"
+            className="text-blue-500 hover:underline"
+          >
+            Image by Freepik
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.freepik.com/free-photo/small-pug-sofa_11043847.htm"
+            className="text-blue-500 hover:underline"
+          >
+            Image by Freepik
+          </a>
+        </li>
+
+        <li>
+          <a
+            href="https://www.freepik.com/free-ai-image/cute-puppy-sitting-grass-enjoying-nature-playful-beauty-generated-by-artificial-intelligence_79687547.htm"
+            className="text-blue-500 hover:underline"
+          >
+            Image by vecstock on Freepik
+          </a>
+        </li>
+      </ul>
+      <p className="text-gray-700 mb-4">
+        All content is used for demonstration purposes only and is not intended
+        for commercial distribution.
+      </p>
+
+      <h2 className="text-xl font-semibold mb-2">Types of Data Collected</h2>
+      <ul className="list-disc pl-6 mb-4">
+        <li>
+          <strong>Personal Information:</strong> This app does not collect
+          personal information like names or email addresses.
+        </li>
+        <li>
+          <strong>Usage Data:</strong> The app may track how users navigate and
+          use the app to demonstrate usage statistics.
+        </li>
+        <li>
+          <strong>Cookies and Tracking Technologies:</strong> This app may use
+          cookies to improve user experience, but no personally identifiable
+          information is stored.
+        </li>
+      </ul>
+
+      <h2 className="text-xl font-semibold mb-2">Types of Data Collected</h2>
+      <ul className="list-disc pl-6 mb-4">
+        <li>
+          <strong>Personal Information:</strong> This app does not collect
+          personal information like names or email addresses.
+        </li>
+        <li>
+          <strong>Usage Data:</strong> The app may track how users navigate and
+          use the app to demonstrate usage statistics.
+        </li>
+        <li>
+          <strong>Cookies and Tracking Technologies:</strong> This app may use
+          cookies to improve user experience, but no personally identifiable
+          information is stored.
+        </li>
+      </ul>
+
+      <h2 className="text-xl font-semibold mb-2">How Data is Used</h2>
+      <p className="text-gray-700 mb-4">
+        Any data collected is used solely for demonstrating app functionality
+        and is not shared with third parties. It helps in understanding user
+        behavior and improving the app's features.
+      </p>
+
+      <h2 className="text-xl font-semibold mb-2">
+        Data Sharing and Disclosure
+      </h2>
+      <p className="text-gray-700 mb-4">
+        No data is shared with third parties. If third-party services (such as
+        analytics tools) are used, they may collect data as per their privacy
+        policies.
+      </p>
+
+      <h2 className="text-xl font-semibold mb-2">Data Security</h2>
+      <p className="text-gray-700 mb-4">
+        The app employs standard security measures to protect data. However, as
+        this is a demo app, it is not intended to handle sensitive or personal
+        information.
+      </p>
+
+      <h2 className="text-xl font-semibold mb-2">User Rights</h2>
+      <p className="text-gray-700 mb-4">
+        Since this is a demonstration app, no real data is collected, stored, or
+        processed. As such, users do not have personal data rights to request
+        access, deletion, or correction.
+      </p>
+
+      <h2 className="text-xl font-semibold mb-2">Contact Information</h2>
+      <p className="text-gray-700 mb-4">
+        If you have any questions or concerns about this Privacy Policy, please
+        contact me at [your-email@example.com].
+      </p>
+
+      <h2 className="text-xl font-semibold mb-2">
+        Disclaimer for Demo Purposes
+      </h2>
+      <p className="text-gray-700 mb-4">
+        This app is for demonstration purposes only and does not collect or
+        process real user data. Any data displayed is purely fictitious or
+        anonymized.
+      </p>
+    </div>
+  );
+};
+
+export default Privacy;
