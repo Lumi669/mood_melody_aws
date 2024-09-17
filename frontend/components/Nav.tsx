e041146249b33b39ad75143eb6120a625429eeca
cmjiwa <wangjinghuan@yahoo.com>
2024-09-17 12:08:36 +0300

    Add songs dropdwon which has allmusic and my playlist page

--- frontend/components/Nav.tsx
+++ frontend/components/Nav.tsx
@@ -9,14 +9,26 @@ import { motion, AnimatePresence } from "framer-motion";
 const Nav: React.FC = () => {
   const [isDropdownOpen, setDropdownOpen] = useState(false);
   const [isTechDropdownOpen, setTechDropdownOpen] = useState(false);
+  const [isSongsDropdownOpen, setSongsDropdownOpen] = useState(false);
 
-  const handleMouseEnter = (
+  const handleAboutMouseEnter = (
     setter: React.Dispatch<React.SetStateAction<boolean>>,
   ) => {
     setter(true);
   };
 
-  const handleMouseLeave = (
+  const handleAboutMouseLeave = (
+    setter: React.Dispatch<React.SetStateAction<boolean>>,
+  ) => {
+    setter(false);
+  };
+  const handleSongsMouseEnter = (
+    setter: React.Dispatch<React.SetStateAction<boolean>>,
+  ) => {
+    setter(true);
+  };
+
+  const handleSongsMouseLeave = (
     setter: React.Dispatch<React.SetStateAction<boolean>>,
   ) => {
     setter(false);
@@ -28,6 +40,14 @@ const Nav: React.FC = () => {
     setTechDropdownOpen(false);
   };
 
+  // Define a union type for the allowed keys
+  type PlaylistKey = "All Music" | "My Playlist";
+
+  // Create a mapping object with the union type keys
+  const urlMapping: Record<PlaylistKey, string> = {
+    "All Music": "allmusic",
+    "My Playlist": "mixtape",
+  };
   return (
     <nav className="sticky top-0 z-50 bg-white shadow-md">
       <div className="container mx-auto flex justify-start items-center space-x-6 p-4">
@@ -43,16 +63,46 @@ const Nav: React.FC = () => {
         </Link>
         {/* Navigation Links */}
         <NavLink href="/">Home</NavLink>
-        <NavLink href="/allmusic">All Music</NavLink>
-        <NavLink href="/mixtape">My Playlist</NavLink>
+
+        {/* Songs Dropdown */}
+        <div
+          className="relative"
+          onMouseEnter={() => handleSongsMouseEnter(setSongsDropdownOpen)}
+          onMouseLeave={() => handleSongsMouseLeave(setSongsDropdownOpen)}
+        >
+          <NavLink href="/allmusic">Songs</NavLink>
+          <AnimatePresence>
+            {isSongsDropdownOpen && (
+              <motion.ul
+                initial={{ opacity: 0, y: -10 }}
+                animate={{ opacity: 1, y: 0 }}
+                exit={{ opacity: 0, y: -10 }}
+                transition={{ duration: 0.2 }}
+                className="absolute left-0 mt-2 w-40 bg-white shadow-lg rounded-md"
+              >
+                {(["All Music", "My Playlist"] as PlaylistKey[]).map((item) => (
+                  <li key={item}>
+                    <Link
+                      href={`/songs/${urlMapping[item]}`} // Use the mapped value
+                      className="block px-4 py-2 hover:bg-gray-100"
+                      onClick={handleLinkClick}
+                    >
+                      {item}
+                    </Link>
+                  </li>
+                ))}
+              </motion.ul>
+            )}
+          </AnimatePresence>
+        </div>
         <NavLink href="/live">Live</NavLink>
         <NavLink href="/test">Test</NavLink>
 
         {/* About Dropdown */}
         <div
           className="relative"
-          onMouseEnter={() => handleMouseEnter(setDropdownOpen)}
-          onMouseLeave={() => handleMouseLeave(setDropdownOpen)}
+          onMouseEnter={() => handleAboutMouseEnter(setDropdownOpen)}
+          onMouseLeave={() => handleAboutMouseLeave(setDropdownOpen)}
         >
           <NavLink href="/about">About</NavLink>
           <AnimatePresence>
@@ -67,8 +117,12 @@ const Nav: React.FC = () => {
                 {/* Tech Dropdown */}
                 <li
                   className="relative"
-                  onMouseEnter={() => handleMouseEnter(setTechDropdownOpen)}
-                  onMouseLeave={() => handleMouseLeave(setTechDropdownOpen)}
+                  onMouseEnter={() =>
+                    handleAboutMouseEnter(setTechDropdownOpen)
+                  }
+                  onMouseLeave={() =>
+                    handleAboutMouseLeave(setTechDropdownOpen)
+                  }
                 >
                   <span className="block px-4 py-2 cursor-pointer hover:bg-gray-100">
                     Tech
