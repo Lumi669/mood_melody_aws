c4ee1acb12b2141d7d256376d38aab11e245f597
cmjiwa <wangjinghuan@yahoo.com>
2024-09-18 21:48:30 +0300

    Implement About page with links for tech

--- frontend/src/app/about/page.tsx
+++ frontend/src/app/about/page.tsx
@@ -1,14 +1,54 @@
 import React from "react";
+import Link from "next/link";
 
 const Aboutpage = () => {
   return (
     <div>
-      <h1 className="text-center mt-10 text-6xl">About This App</h1>
-      <p className="indent-8 m-20 text-xl">
-        Welcome to my About page. Here, you&apos;ll find information about the
-        app. Explore corresponding sections for more details on the technology,
-        privacy policy, and testimonials and analysis.
-      </p>
+      <h1 className="text-center mt-8 text-4xl">About This App</h1>
+      <div className="h-screen overflow-y-scroll px-4 pb-80 p-10">
+        <div className="max-w-5xl mx-auto">
+          <p className="indent-8 m-10 text-xl leading-relaxed">
+            This application serves as a demonstration of my full-stack
+            development expertise. It highlights my ability to design and
+            develop user-friendly, responsive web applications, build efficient
+            back-end solutions and APIs, and integrate modern front-end
+            technologies. Additionally, it features a CI/CD pipeline and DevOps
+            practices, ensuring seamless and automated deployment. Utilizing AWS
+            services for hosting and deployment, this project also incorporates
+            AI-driven functionalities.
+          </p>
+
+          <p className="mt-20 text-center text-2xl mb-5">
+            For detailed technical information, check:
+          </p>
+          <ul className="list-disc list-inside text-center">
+            <li className="mb-4">
+              <Link
+                href="/about/tech/architecture"
+                className="text-blue-500 font-bold no-underline hover:text-blue-700 text-xl"
+              >
+                Architecture
+              </Link>
+            </li>
+            <li className="mb-4">
+              <Link
+                href="/about/tech/cicd"
+                className="text-blue-500 font-bold no-underline hover:text-blue-700 text-xl"
+              >
+                CI/CD
+              </Link>
+            </li>
+            <li className="mb-4">
+              <Link
+                href="/about/tech/techstack"
+                className="text-blue-500 font-bold no-underline hover:text-blue-700 text-xl"
+              >
+                Tech Stack
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
