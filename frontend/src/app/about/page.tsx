c28b891edcf01eecfbb128bd8767199deb318f9e
cmjiwa <wangjinghuan@yahoo.com>
2024-09-19 17:29:52 +0300

    Add movable arrow on Contact me button of About page

--- frontend/src/app/about/page.tsx
+++ frontend/src/app/about/page.tsx
@@ -60,9 +60,12 @@ const Aboutpage = () => {
             <button
               type="button"
               onClick={() => router.push("/contact")}
-              className="border-2 border-red-500 bg-yellow-200 active-button-style whitespace-nowrap w-auto px-8 py-3"
+              className="border-2 border-red-500 bg-yellow-100 text-black px-6 py-3 rounded-full shadow-md hover:bg-yellow-200 hover:shadow-lg active:bg-yellow-300 active:shadow-none focus:outline-none focus:ring-4 focus:ring-yellow-100 transition-all duration-200 ease-in-out flex items-center group"
             >
-              Contact Me
+              Contact me
+              <span className="ml-2 transform transition-transform duration-300 ease-in-out group-hover:translate-x-2">
+                &rarr;
+              </span>
             </button>
           </div>
         </div>
