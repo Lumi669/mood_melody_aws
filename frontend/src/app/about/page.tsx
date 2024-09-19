6ec7565aaab83fbb5d0d42e5ed6cfdf1f349e1f5
cmjiwa <wangjinghuan@yahoo.com>
2024-09-19 17:40:56 +0300

    Remove red border of Contact me button on About page

--- frontend/src/app/about/page.tsx
+++ frontend/src/app/about/page.tsx
@@ -60,7 +60,7 @@ const Aboutpage = () => {
             <button
               type="button"
               onClick={() => router.push("/contact")}
-              className="border-2 border-red-500 bg-yellow-100 text-black px-6 py-3 rounded-full shadow-md hover:bg-yellow-200 hover:shadow-lg active:bg-yellow-300 active:shadow-none focus:outline-none focus:ring-4 focus:ring-yellow-100 transition-all duration-200 ease-in-out flex items-center group"
+              className="border-2  bg-yellow-100 text-black px-6 py-3 rounded-full shadow-md hover:bg-yellow-200 hover:shadow-lg active:bg-yellow-300 active:shadow-none focus:outline-none focus:ring-4 focus:ring-yellow-100 transition-all duration-200 ease-in-out flex items-center group"
             >
               Contact me
               <span className="ml-2 transform transition-transform duration-300 ease-in-out group-hover:translate-x-2">
