a1b1c32c4d16feacc285434dc314ccf973753bdc
cmjiwa <wangjinghuan@yahoo.com>
2024-09-23 22:15:30 +0300

    Add clickable email and linkedIn on contact page

--- frontend/src/app/contact/page.tsx
+++ frontend/src/app/contact/page.tsx
@@ -1,156 +1,10 @@
-// // "use client";
-
-// // import React from "react";
-
-// // const ContactPage = () => {
-// //   return (
-// //     <div className="min-h-screen overflow-y-auto bg-gray-50">
-// //       {/* Contact Section */}
-// //       <div className="py-16 bg-gradient-to-r from-orange-500 to-black via-white from-20% via-70% to-90%">
-// //         <div className="max-w-5xl mx-auto px-4">
-// //           <div className="grid grid-cols-1 md:grid-cols-2 gap-8 items-center">
-// //             {/* Text Content */}
-// //             <div>
-// //               <h1 className="text-4xl font-bold mb-4 text-white">Contact me</h1>
-// //               <p className="text-lg text-gray-100 mb-4">
-// //                 Interested in collaborating? If you&apos;re looking for a
-// //                 passionate developer to join your team, whether full-time or as
-// //                 a subcontractor, or if you&apos;d like to discuss potential
-// //                 cooperation on a project, I&apos;d love to hear from you!
-// //               </p>
-// //               <p className="text-lg text-gray-100">
-// //                 Feel free to reach out using the form below or directly via your
-// //                 preferred contact method, like email or LinkedIn.
-// //               </p>
-// //             </div>
-// //             {/* Personal Photo */}
-// //             <div className="flex justify-center">
-// //               <img
-// //                 src="/profile.webp"
-// //                 alt="Jinghuan Wang"
-// //                 className="w-64 h-64 rounded-full border-4 border-white shadow-lg"
-// //               />
-// //             </div>
-// //           </div>
-// //         </div>
-// //       </div>
-
-// //       {/* Additional Content Section */}
-// //       <div className="max-w-5xl mx-auto mt-10 px-4 py-8 bg-white">
-// //         <h2 className="text-2xl font-bold mb-6">Additional Content</h2>
-// //         <p className="text-gray-700 mb-4">
-// //           This section can be used to include additional information such as a
-// //           contact form, testimonials, or anything else you&apos;d like to share.
-// //         </p>
-// //         <p className="text-gray-700 mb-4">
-// //           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec
-// //           odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla
-// //           quis sem at nibh elementum imperdiet.
-// //         </p>
-// //         <p className="text-gray-700 mb-4">
-// //           Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue
-// //           semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla.
-// //         </p>
-// //         <p className="text-gray-700 mb-4">
-// //           Class aptent taciti sociosqu ad litora torquent per conubia nostra,
-// //           per inceptos himenaeos. Curabitur sodales ligula in libero.
-// //         </p>
-// //         <p className="text-gray-700 mb-4">
-// //           Sed dignissim lacinia nunc. Curabitur tortor. Pellentesque nibh.
-// //           Aenean quam. In scelerisque sem at dolor. Maecenas mattis.
-// //         </p>
-// //         <p className="text-gray-700 mb-4">
-// //           Sed convallis tristique sem. Proin ut ligula vel nunc egestas
-// //           porttitor. Morbi lectus risus, iaculis vel, suscipit quis, luctus non,
-// //           massa.
-// //         </p>
-// //         <p className="text-gray-700 mb-4">
-// //           Fusce ac turpis quis ligula lacinia aliquet. Mauris ipsum. Nulla metus
-// //           metus, ullamcorper vel, tincidunt sed, euismod in, nibh. Quisque
-// //           volutpat condimentum velit.
-// //         </p>
-// //       </div>
-// //     </div>
-// //   );
-// // };
-
-// // export default ContactPage;
-// "use client";
-
-// import React from "react";
-
-// const ContactPage = () => {
-//   return (
-//     <div className="h-screen overflow-y-scroll px-4 pb-80 p-10">
-//       <div className="max-w-5xl mx-auto">
-//         <h1 className="text-4xl font-bold mb-4 text-white">Contact me</h1>
-//         <p className="text-lg text-gray-100 mb-4">
-//           Interested in collaborating? If you&apos;re looking for a passionate
-//           developer to join your team, whether full-time or as a subcontractor,
-//           or if you&apos;d like to discuss potential cooperation on a project,
-//           I&apos;d love to hear from you!
-//         </p>
-//         <p className="text-lg text-gray-100">
-//           Feel free to reach out using the form below or directly via your
-//           preferred contact method, like email or LinkedIn.
-//         </p>
-
-//         {/* Personal Photo */}
-//         <div className="flex justify-center">
-//           <img
-//             src="/profile.webp"
-//             alt="Jinghuan Wang"
-//             className="w-64 h-64 rounded-full border-4 border-white shadow-lg"
-//           />
-//         </div>
-
-//         {/* Additional Content Section */}
-//         <div className="max-w-5xl mx-auto mt-10 px-4 py-8 bg-white">
-//           <h2 className="text-2xl font-bold mb-6">Additional Content</h2>
-//           <p className="text-gray-700 mb-4">
-//             This section can be used to include additional information such as a
-//             contact form, testimonials, or anything else you&apos;d like to
-//             share.
-//           </p>
-//           <p className="text-gray-700 mb-4">
-//             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec
-//             odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla
-//             quis sem at nibh elementum imperdiet.
-//           </p>
-//           <p className="text-gray-700 mb-4">
-//             Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue
-//             semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla.
-//           </p>
-//           <p className="text-gray-700 mb-4">
-//             Class aptent taciti sociosqu ad litora torquent per conubia nostra,
-//             per inceptos himenaeos. Curabitur sodales ligula in libero.
-//           </p>
-//           <p className="text-gray-700 mb-4">
-//             Sed dignissim lacinia nunc. Curabitur tortor. Pellentesque nibh.
-//             Aenean quam. In scelerisque sem at dolor. Maecenas mattis.
-//           </p>
-//           <p className="text-gray-700 mb-4">
-//             Sed convallis tristique sem. Proin ut ligula vel nunc egestas
-//             porttitor. Morbi lectus risus, iaculis vel, suscipit quis, luctus
-//             non, massa.
-//           </p>
-//           <p className="text-gray-700 mb-4">
-//             Fusce ac turpis quis ligula lacinia aliquet. Mauris ipsum. Nulla
-//             metus metus, ullamcorper vel, tincidunt sed, euismod in, nibh.
-//             Quisque volutpat condimentum velit.
-//           </p>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default ContactPage;
 "use client";
 
 import React from "react";
 
 const ContactPage = () => {
+  const email = process.env.NEXT_PUBLIC_PERSONAL_EMAIL;
+
   return (
     <div className="h-screen overflow-y-scroll px-4 pb-80">
       <div className="max-w-5xl mx-auto">
@@ -168,7 +22,22 @@ const ContactPage = () => {
               </p>
               <p className="text-lg text-gray-100">
                 Feel free to reach out using the form below or directly via your
-                preferred contact method, like email or LinkedIn.
+                preferred contact method, like{" "}
+                <a
+                  href={`mailto:${email}`}
+                  className="text-blue-500 hover:underline"
+                >
+                  {email}
+                </a>{" "}
+                or{" "}
+                <a
+                  href="https://www.linkedin.com/in/jinghuanwang/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-blue-500 hover:text-blue-400 "
+                >
+                  LinkedIn
+                </a>
               </p>
             </div>
             {/* Personal Photo */}
