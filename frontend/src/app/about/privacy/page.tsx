e02c7ba9ac1d69d77bce08fd25253fa4663518fe
cmjiwa <wangjinghuan@yahoo.com>
2024-09-23 19:17:48 +0300

    Get rid of redundant contents from privacy page

--- frontend/src/app/about/privacy/page.tsx
+++ frontend/src/app/about/privacy/page.tsx
@@ -1,8 +1,7 @@
 "use client";
 
-// require("dotenv").config();
-
 import React from "react";
+import Link from "next/link";
 import { useMedia } from "@context/MediaContext";
 import { getTextColor } from "@utils/getTextColor";
 
@@ -11,7 +10,7 @@ const PrivacyPage = () => {
   const email = process.env.NEXT_PUBLIC_PERSONAL_EMAIL;
 
   return (
-    <div className="h-screen overflow-y-scroll px-4  pb-80 p-10">
+    <div className="h-screen overflow-y-scroll px-4 pb-80 p-10">
       <div className="max-w-5xl mx-auto">
         <h1
           className={`text-3xl font-extrabold mb-4 ${getTextColor(
@@ -25,8 +24,7 @@ const PrivacyPage = () => {
         <p className="text-gray-700 mb-4">
           This Privacy Policy explains how this app handles your data. Please
           note that this app is a demonstration project created to showcase my
-          development skills, and it is not intended for commercial use.
-          Therefore, it does not actively collect or store personal data.
+          development skills and is not intended for commercial use.
         </p>
 
         <h2 className="text-xl font-semibold mb-2">
@@ -39,112 +37,144 @@ const PrivacyPage = () => {
         <ul className="list-disc pl-6 mb-4">
           <li>
             Animations from{" "}
-            <a
-              href="https://www.veed.io/"
-              className="text-blue-500 hover:underline"
-              target="_blank"
-            >
-              Veed.io
-            </a>
+            <Link href="https://www.veed.io/" legacyBehavior>
+              <a
+                className="text-blue-500 hover:underline"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Veed.io
+              </a>
+            </Link>
             , used according to their licensing terms.
           </li>
           <li>
             Music from{" "}
-            <a
-              href="https://artlist.io/"
-              className="text-blue-500 hover:underline"
-              target="_blank"
-            >
-              Artlist
-            </a>
+            <Link href="https://artlist.io/" legacyBehavior>
+              <a
+                className="text-blue-500 hover:underline"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Artlist
+              </a>
+            </Link>
             , all song titles and artists are mentioned on the music page.
           </li>
         </ul>
 
         <p className="text-gray-700 mb-4">
           This app includes images sourced from{" "}
-          <a
-            href="https://www.freepik.com"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-blue-500 hover:underline"
-          >
-            Freepik
-          </a>
+          <Link href="https://www.freepik.com" legacyBehavior>
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-blue-500 hover:underline"
+            >
+              Freepik
+            </a>
+          </Link>
           . These images are used under Freepik&apos;s free license with proper
-          attribution to the original authors:
+          attribution to the original authors.
         </p>
         <ul className="list-disc pl-6 mb-4">
           <li>
-            <a
+            <Link
               href="https://www.freepik.com/free-photo/adorable-little-poodle-with-cute-bunny-ears-beige-surface_15672270.htm"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-blue-500 hover:underline"
+              legacyBehavior
             >
-              Image by wirestock on Freepik
-            </a>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-blue-500 hover:underline"
+              >
+                Image by wirestock on Freepik
+              </a>
+            </Link>
           </li>
           <li>
-            <a
+            <Link
               href="https://www.freepik.com/free-photo/happy-beagle-with-droopy-ears-blue-background_135010608.htm"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-blue-500 hover:underline"
+              legacyBehavior
             >
-              Image by rorozoa on Freepik
-            </a>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-blue-500 hover:underline"
+              >
+                Image by rorozoa on Freepik
+              </a>
+            </Link>
           </li>
           <li>
-            <a
+            <Link
               href="https://www.freepik.com/free-photo/medium-shot-friends-eating-pizza_138239739.htm"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-blue-500 hover:underline"
+              legacyBehavior
             >
-              Image by Drazen Zigic on Freepik
-            </a>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-blue-500 hover:underline"
+              >
+                Image by Drazen Zigic on Freepik
+              </a>
+            </Link>
           </li>
           <li>
-            <a
+            <Link
               href="https://www.freepik.com/free-photo/cute-puppy-sitting-grass-enjoying-nature-playful-beauty-generated-by-artificial-intelligence_79687547.htm"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-blue-500 hover:underline"
+              legacyBehavior
             >
-              Image by upklyak on Freepik
-            </a>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-blue-500 hover:underline"
+              >
+                Image by upklyak on Freepik
+              </a>
+            </Link>
           </li>
           <li>
-            <a
+            <Link
               href="https://www.freepik.com/free-photo/close-up-adorable-dog-laying-blanket_10422541.htm"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-blue-500 hover:underline"
+              legacyBehavior
             >
-              Image by Freepik
-            </a>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-blue-500 hover:underline"
+              >
+                Image by Freepik
+              </a>
+            </Link>
           </li>
           <li>
-            <a
+            <Link
               href="https://www.freepik.com/free-photo/small-pug-sofa_11043847.htm"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-blue-500 hover:underline"
+              legacyBehavior
             >
-              Image by Freepik
-            </a>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-blue-500 hover:underline"
+              >
+                Image by Freepik
+              </a>
+            </Link>
           </li>
-
           <li>
-            <a
+            <Link
               href="https://www.freepik.com/free-ai-image/cute-puppy-sitting-grass-enjoying-nature-playful-beauty-generated-by-artificial-intelligence_79687547.htm"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-blue-500 hover:underline"
+              legacyBehavior
             >
-              Image by vecstock on Freepik
-            </a>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-blue-500 hover:underline"
+              >
+                Image by vecstock on Freepik
+              </a>
+            </Link>
           </li>
         </ul>
         <p className="text-gray-700 mb-4">
@@ -152,70 +182,53 @@ const PrivacyPage = () => {
           intended for commercial distribution.
         </p>
 
-        <h2 className="text-xl font-semibold mb-2">Types of Data Collected</h2>
+        {/* The rest of your content remains the same */}
+        <h2 className="text-xl font-semibold mb-2">
+          Data Collection and Usage
+        </h2>
         <ul className="list-disc pl-6 mb-4">
           <li>
-            <strong>Personal Information:</strong> This app may collect personal
-            information such as usernames and email addresses during the user
-            registration process. This information is used solely for creating
-            and managing user accounts, and to provide a personalized experience
-            within the app.
+            <strong>Personal Information:</strong> This app may collect
+            usernames and email addresses during the user registration process.
+            This information is used solely for creating and managing user
+            accounts and providing a personalized experience.
           </li>
           <li>
-            <strong>Usage Data:</strong> The app may track how users navigate
-            and use the app to demonstrate usage statistics.
+            <strong>Usage Data:</strong> The app may track user activity to
+            demonstrate usage statistics.
           </li>
           <li>
             <strong>Cookies and Tracking Technologies:</strong> This app may use
-            cookies to improve user experience, but no personally identifiable
-            information is stored.
+            cookies to improve the user experience, but no personally
+            identifiable information is stored.
           </li>
         </ul>
 
-        <h2 className="text-xl font-semibold mb-2">How Data is Used</h2>
+        <h2 className="text-xl font-semibold mb-2">
+          Data Security and Sharing
+        </h2>
         <p className="text-gray-700 mb-4">
-          Any data collected is used solely for demonstrating app functionality
-          and is not shared with third parties. It helps in understanding user
-          behavior and improving the app&apos;s features.
+          Any data collected is used solely for demonstrating app functionality.
+          I do not share your personal information with third parties for
+          marketing or advertising purposes. However, I may use the following
+          third-party service to improve the app: Google Analytics which tracks
+          anonymized user behavior to help me improve the app experience. For
+          more information on how this third-party service handle your data,
+          please review their respective privacy policies.
         </p>
 
         <h2 className="text-xl font-semibold mb-2">
-          Data Sharing and Disclosure
+          User Rights and Contact Information
         </h2>
-        <p className="text-gray-700 mb-4">
-          No personal data is shared with third parties for marketing or
-          advertising purposes. However, if third-party services (such as
-          analytics tools) are used, they may collect and process data in
-          accordance with their own privacy policies. Please review the privacy
-          policies of these third-party services to understand how they handle
-          your data.
-        </p>
-
-        <h2 className="text-xl font-semibold mb-2">Data Security</h2>
-        <p className="text-gray-700 mb-4">
-          The app employs standard security measures to protect data. However,
-          as this is a demo app, it is not intended to handle sensitive or
-          personal information.
-        </p>
-
-        <h2 className="text-xl font-semibold mb-2">User Rights</h2>
         <p className="text-gray-700 mb-4">
           Users have the right to access, update, or delete their personal
           information, such as their username and email address, stored within
           this app. If you would like to exercise these rights or have any
-          concerns about how your data is being handled, please contact us at{" "}
+          concerns about how your data is being handled, please contact me at{" "}
           {email}.
         </p>
 
-        <h2 className="text-xl font-semibold mb-2">Contact Information</h2>
-        <p className="text-gray-700 mb-4">
-          If you have any questions or concerns about this Privacy Policy,
-          please contact me at {email}.
-        </p>
-
-        <h2 className="text-xl font-semibold mb-2">
-          Disclaimer for Demo Purposes
-        </h2>
+        <h2 className="text-xl font-semibold mb-2">Disclaimer</h2>
         <p className="text-gray-700 mb-4">
           This app is for demonstration purposes. It may collect and store real
           user data, specifically usernames and email addresses, but no other
