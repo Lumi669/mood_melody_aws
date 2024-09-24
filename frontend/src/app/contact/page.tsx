f99dff1d81cda9eeb06d31682b84bb1b79fbac48
cmjiwa <wangjinghuan@yahoo.com>
2024-09-24 19:51:42 +0300

    Contact page, gradient profile photo part span the whole width of ui

--- frontend/src/app/contact/page.tsx
+++ frontend/src/app/contact/page.tsx
@@ -1,70 +1,3 @@
-// "use client";
-
-// import React from "react";
-// import ContactForm from "@components/ContactForm";
-// import InfoSections from "./InfoSections";
-
-// const ContactPage = () => {
-//   const email = process.env.NEXT_PUBLIC_PERSONAL_EMAIL;
-
-//   return (
-//     <div className="h-screen overflow-y-scroll px-4 pb-80">
-//       <div className="max-w-5xl mx-auto">
-//         {/* Contact Section with Gradient Background */}
-//         <div className="py-16 px-4 bg-gradient-to-r from-orange-500 to-black via-white from-20% via-70% to-90%">
-//           <div className="grid grid-cols-1 md:grid-cols-2 gap-8 items-center">
-//             {/* Text Content */}
-//             <div>
-//               <h1 className="text-4xl font-bold mb-4 text-white">Contact me</h1>
-//               <p className="text-lg text-gray-100 mb-4">
-//                 Interested in collaborating? If you&apos;re looking for a
-//                 passionate developer to join your team, whether full-time or as
-//                 a subcontractor, or if you&apos;d like to discuss potential
-//                 cooperation on a project, I&apos;d love to hear from you!
-//               </p>
-//               <p className="text-lg text-gray-100">
-//                 Feel free to reach out using the form below or directly via your
-//                 preferred contact method, like{" "}
-//                 <a
-//                   href={`mailto:${email}`}
-//                   className="text-blue-500 hover:underline"
-//                 >
-//                   {email}
-//                 </a>{" "}
-//                 or{" "}
-//                 <a
-//                   href="https://www.linkedin.com/in/jinghuanwang/"
-//                   target="_blank"
-//                   rel="noopener noreferrer"
-//                   className="text-blue-500 hover:text-blue-400 "
-//                 >
-//                   LinkedIn
-//                 </a>
-//               </p>
-//             </div>
-//             {/* Personal Photo */}
-//             <div className="flex justify-center">
-//               <img
-//                 src="/profile.webp"
-//                 alt="Jinghuan Wang"
-//                 className="w-64 h-64 rounded-full border-4 border-white shadow-lg"
-//               />
-//             </div>
-//           </div>
-//         </div>
-
-//         {/* Additional Content Section */}
-//         <div className="max-w-5xl mx-auto mt-10 px-4 py-8 bg-white">
-//           <ContactForm />
-//         </div>
-//       </div>
-//       <div></div>
-//     </div>
-//   );
-// };
-
-// export default ContactPage;
-
 "use client";
 
 import React from "react";
@@ -75,13 +8,13 @@ const ContactPage = () => {
   const email = process.env.NEXT_PUBLIC_PERSONAL_EMAIL;
 
   return (
-    <div className="h-screen overflow-y-scroll px-4 pb-80">
-      <div className="max-w-5xl mx-auto">
-        {/* Contact Section with Gradient Background */}
-        <div className="py-16 px-4 bg-gradient-to-r from-orange-500 to-black via-white from-20% via-70% to-90%">
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-8 items-center">
+    <div className="h-screen overflow-y-scroll pb-80">
+      {/* Contact Section with Gradient Background */}
+      <div className="-mx-4">
+        <div className="py-16 px-0 bg-gradient-to-r from-orange-500 to-black via-white from-20% via-70% to-90%">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-8 items-center max-w-7xl mx-auto">
             {/* Text Content */}
-            <div>
+            <div className="px-4 md:px-0">
               <h1 className="text-4xl font-bold mb-4 text-white">Contact me</h1>
               <p className="text-lg text-gray-100 mb-4">
                 Interested in collaborating? If you&apos;re looking for a
@@ -110,7 +43,7 @@ const ContactPage = () => {
               </p>
             </div>
             {/* Personal Photo */}
-            <div className="flex justify-center">
+            <div className="flex justify-center px-4 md:px-0">
               <img
                 src="/profile.webp"
                 alt="Jinghuan Wang"
@@ -119,16 +52,16 @@ const ContactPage = () => {
             </div>
           </div>
         </div>
+      </div>
 
-        {/* Contact Form Section */}
-        <div className="max-w-5xl mx-auto mt-10 px-4 py-8 ">
-          <ContactForm />
-        </div>
+      {/* Contact Form Section */}
+      <div className="max-w-5xl mx-auto mt-10 px-4 py-8 ">
+        <ContactForm />
+      </div>
 
-        {/* Info Sections */}
-        <div className="max-w-5xl mx-auto  px-4 py-2 ">
-          <InfoSections /> {/* Include the InfoSections component */}
-        </div>
+      {/* Info Sections */}
+      <div className="max-w-5xl mx-auto  px-4 py-2 ">
+        <InfoSections /> {/* Include the InfoSections component */}
       </div>
     </div>
   );
