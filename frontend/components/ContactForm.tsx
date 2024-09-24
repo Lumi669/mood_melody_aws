25cdb46b84b3699e4bbc2678fdf11166479b4ff6
cmjiwa <wangjinghuan@yahoo.com>
2024-09-24 19:49:39 +0300

    Contact page, gradient profile photos not full width, contact form, infor sections

--- frontend/components/ContactForm.tsx
+++ frontend/components/ContactForm.tsx
@@ -235,29 +235,29 @@ const ContactForm: React.FC = () => {
       {/* Additional Information Section */}
       <div className="mt-12 grid grid-cols-1 md:grid-cols-3 gap-8">
         {/* Section 1 */}
-        <div className="text-center">
+        {/* <div className="text-center">
           <h2 className="font-bold text-lg">My Name</h2>
           <p className="mt-2">Jinghuan Wang</p>
           <h2 className="font-bold text-lg mt-4">My Title</h2>
           <p className="mt-2">Software Developer</p>
-        </div>
+        </div> */}
 
         {/* Section 2 */}
-        <div className="text-center">
+        {/* <div className="text-center">
           <h2 className="font-bold text-lg">My Interested Roles</h2>
           <p className="mt-2">Fullstack Developer</p>
           <p className="mt-1">Frontend Developer</p>
           <p className="mt-1">Backend Developer</p>
           <p className="mt-1">AWS and Software Dev Related Roles</p>
-        </div>
+        </div> */}
 
         {/* Section 3 */}
-        <div className="text-center">
+        {/* <div className="text-center">
           <h2 className="font-bold text-lg">My Working Place</h2>
           <p className="mt-2">Tampere, Finland: Onsite, Hybrid</p>
           <p className="mt-1">Other Places in Finland: Remote</p>
           <p className="mt-1">Other Countries: Remote</p>
-        </div>
+        </div> */}
       </div>
     </div>
   );
