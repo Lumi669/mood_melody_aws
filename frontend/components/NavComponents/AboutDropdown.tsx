5a7fcc1d992192c6008a4a1ebafc2126c1476810
cmjiwa <wangjinghuan@yahoo.com>
2024-09-26 00:03:04 +0300

    Mobile version, about dropdown closer on y-axis to About page name

--- frontend/components/NavComponents/AboutDropdown.tsx
+++ frontend/components/NavComponents/AboutDropdown.tsx
@@ -45,7 +45,7 @@ const AboutDropdown: React.FC<AboutDropdownProps> = ({
           animate={{ opacity: 1, y: 0 }}
           exit={{ opacity: 0, y: -10 }}
           transition={{ duration: 0.2 }}
-          className="absolute left-0 mt-2 w-40 bg-white shadow-lg rounded-md z-50"
+          className="absolute left-0 w-40 bg-white shadow-lg rounded-md z-50"
         >
           {/* Tech Menu Item with Nested Dropdown */}
           <li
