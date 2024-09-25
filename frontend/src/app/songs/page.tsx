4d05d4ef71382f6426d33e052837a3681d091fa7
cmjiwa <wangjinghuan@yahoo.com>
2024-09-25 13:16:53 +0300

    Songs page, get rid of contrast issues of the two buttons from Accessiblity Insight check

--- frontend/src/app/songs/page.tsx
+++ frontend/src/app/songs/page.tsx
@@ -11,7 +11,6 @@ const SongsPage = () => {
     <div className="relative flex flex-col items-center justify-center h-screen">
       {/* Wrapper for the content to control positioning */}
       <div className="flex flex-col items-center justify-center -mt-40">
-        {" "}
         <h1
           className={`text-5xl font-extrabold mb-6 drop-shadow-lg ${getTextColor(
             isRed,
@@ -42,12 +41,12 @@ const SongsPage = () => {
         {/* Button area styled with a cute look */}
         <div className="flex justify-center gap-6">
           <Link href="/songs/allmusic">
-            <div className="bg-blue-300 text-white px-8 py-4 rounded-full underlined hover:bg-blue-400 transition text-lg font-semibold shadow-md cursor-pointer">
+            <div className="bg-[#5a799c] text-white px-8 py-4 rounded-full hover:bg-blue-400 transition text-lg font-semibold shadow-md cursor-pointer">
               All Music
             </div>
           </Link>
           <Link href="/songs/mixtape">
-            <div className="bg-purple-300 text-white px-8 py-4 rounded-full hover:bg-purple-400 transition text-lg font-semibold shadow-lg cursor-pointer">
+            <div className="bg-[#7e6894] text-white px-8 py-4 rounded-full hover:bg-purple-400 transition text-lg font-semibold shadow-lg cursor-pointer">
               Your Played List
             </div>
           </Link>
@@ -56,8 +55,7 @@ const SongsPage = () => {
       {/* Decorative shapes for a playful look */}
       <div className="absolute top-10 right-10 w-40 h-40 bg-pink-200 rounded-full opacity-50"></div>
       <div className="absolute bottom-32 left-20 w-32 h-32 bg-purple-200 rounded-full opacity-50"></div>
-      <div className="absolute bottom-40 right-30 w-24 h-24 bg-blue-200 rounded-full opacity-40"></div>{" "}
-      {/* Added a new decorative circle */}
+      <div className="absolute bottom-40 right-30 w-24 h-24 bg-blue-200 rounded-full opacity-40"></div>
     </div>
   );
 };
