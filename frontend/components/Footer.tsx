6ccc752c0c1bfb364eaa3ed4aaa481900ba60c22
cmjiwa <wangjinghuan@yahoo.com>
2024-09-25 14:24:21 +0300

    Add my app name to footer

--- frontend/components/Footer.tsx
+++ frontend/components/Footer.tsx
@@ -1,24 +1,9 @@
-// import React from "react";
-
-// const Footer: React.FC = () => {
-//   return (
-//     <footer className="fixed bottom-0 left-0 w-full bg-gray-800 text-white py-4 text-center z-50">
-//       <p>&copy; {new Date().getFullYear()} Mood Melody. All rights reserved.</p>
-//     </footer>
-//   );
-// };
-
-// export default Footer;
-
-// components/Footer.tsx
 import React from "react";
 
 const Footer: React.FC = () => {
   return (
     <footer className="fixed bottom-0 left-0 w-full bg-gray-800 text-white py-4 text-center z-10">
-      <p>
-        &copy; {new Date().getFullYear()} Your App Name. All rights reserved.
-      </p>
+      <p>&copy; {new Date().getFullYear()} Mood melody. All rights reserved.</p>
     </footer>
   );
 };
