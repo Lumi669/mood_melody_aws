62e5e391156c6ad146c294c26c1148bbc0f2cae6
cmjiwa <wangjinghuan@yahoo.com>
2024-09-26 13:46:00 +0300

    Style to rearrange homepage for mobile

--- frontend/components/Greeting.tsx
+++ frontend/components/Greeting.tsx
@@ -1,22 +1,44 @@
+// import React from "react";
+// import Image from "next/image";
+
+// const Greeting = () => {
+//   return (
+//     <div>
+//       <div className="flex justify-center">
+//         <Image
+//           src="/dancing-girl-removebg.webp"
+//           alt="Description of the image"
+//           width={300}
+//           height={300}
+//           className="rounded-lg"
+//         />
+
+//         <h1 className="m-10 p-20 bg-yellow-100 text-4xl font-mono text-[#1a9f4b] font-bold tracking-wide text-center">
+//           Hi there, How do you feel today :D ?
+//         </h1>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Greeting;
 import React from "react";
 import Image from "next/image";
 
 const Greeting = () => {
   return (
-    <div>
-      <div className="flex justify-center">
-        <Image
-          src="/dancing-girl-removebg.webp"
-          alt="Description of the image"
-          width={300}
-          height={300}
-          className="rounded-lg"
-        />
+    <div className="flex flex-col md:flex-row items-center justify-center md:space-x-4 space-y-4 md:space-y-0 p-4">
+      <Image
+        src="/dancing-girl-removebg.webp"
+        alt="Description of the image"
+        width={300}
+        height={300}
+        className="rounded-lg"
+      />
 
-        <h1 className="m-10 p-20 bg-yellow-100 text-4xl font-mono text-[#1a9f4b] font-bold tracking-wide text-center">
-          Hi there, How do you feel today :D ?
-        </h1>
-      </div>
+      <h1 className="bg-yellow-100 text-2xl md:text-4xl font-mono text-[#1a9f4b] font-bold tracking-wide text-center p-4 rounded-lg">
+        Hi there, How do you feel today :D ?
+      </h1>
     </div>
   );
 };
--- frontend/components/Sentimentanalysis.tsx
+++ frontend/components/Sentimentanalysis.tsx
@@ -1,3 +1,105 @@
+// "use client";
+
+// import React, { useState, useEffect } from "react";
+
+// interface SentimentAnalysisPageProps {
+//   onSentimentAnalyzed: (message: string) => void;
+//   playMusic: (
+//     mood: "happy" | "sad" | "calm",
+//     source: "button" | "analysis",
+//   ) => void;
+// }
+
+// export default function SentimentAnalysisPage({
+//   onSentimentAnalyzed,
+//   playMusic,
+// }: SentimentAnalysisPageProps) {
+//   const [text, setText] = useState("");
+//   const [sentiment, setSentiment] = useState<string | null>(null);
+//   const [isLoading, setIsLoading] = useState<boolean>(false);
+//   const maxChars = 90;
+
+//   const handleTextChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+//     const newText = e.target.value;
+//     if (newText.length <= maxChars) {
+//       setText(newText);
+//     }
+//   };
+
+//   const analyzeSentiment = async () => {
+//     setIsLoading(true); // Start loading
+
+//     try {
+//       const response = await fetch("/api/sentimentanalysis", {
+//         method: "POST",
+//         headers: {
+//           "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify({ text }),
+//       });
+
+//       if (response.ok) {
+//         const data = await response.json();
+//         console.log("Sentiment analysis result:", data.sentiment);
+//         setSentiment(data.sentiment);
+//       } else {
+//         setSentiment("Error analyzing sentiment");
+//       }
+//     } catch (error) {
+//       console.error("Error fetching sentiment analysis:", error);
+//       setSentiment("Error analyzing sentiment");
+//     } finally {
+//       setIsLoading(false); // End loading
+//     }
+//   };
+
+//   useEffect(() => {
+//     if (sentiment) {
+//       // Only trigger this effect when sentiment is updated
+//       let sentimentMessage = "";
+
+//       if (sentiment === "POSITIVE") {
+//         sentimentMessage =
+//           "You seem happy; here's a happy song and image for you.";
+//         playMusic("happy", "analysis"); // Play music directly
+//       } else if (sentiment === "NEGATIVE") {
+//         sentimentMessage =
+//           "It seems you are not happy; here's a sad song and image matching your feelings.";
+//         playMusic("sad", "analysis");
+//       } else if (sentiment === "MIX" || sentiment === "NEUTRAL") {
+//         sentimentMessage =
+//           "It is hard to tell clearly; maybe you feel peaceful. Check the music and image for you.";
+//         playMusic("calm", "analysis");
+//       }
+
+//       onSentimentAnalyzed(sentimentMessage); // Set message only once after sentiment is analyzed
+//     }
+//   }, [sentiment, playMusic, onSentimentAnalyzed]); // Trigger effect when sentiment is updated
+
+//   return (
+//     <div className="p-5 max-w-3xl mx-auto grid justify-items-stretch ">
+//       <div className="relative flex items-start">
+//         <textarea
+//           value={text}
+//           onChange={handleTextChange}
+//           placeholder="Enter text to analyze your sentiment"
+//           rows={4}
+//           className="flex-grow tracking-wide p-3 border border-gray-300 rounded mb-4 ml-6 resize-none w-full" // Make textarea take full width
+//         />
+//         <div className="absolute bottom-1 right-2 text-gray-500 text-sm">
+//           {maxChars - text.length}
+//         </div>
+//         <button
+//           onClick={analyzeSentiment}
+//           className="bg-[#326ed1] text-white py-2 px-4 rounded-full hover:bg-[#2758a8] transition-colors ml-8"
+//           disabled={isLoading}
+//         >
+//           {isLoading ? "Checking..." : "Check my mood"}
+//         </button>
+//       </div>
+//     </div>
+//   );
+// }
 "use client";
 
 import React, { useState, useEffect } from "react";
@@ -27,7 +129,7 @@ export default function SentimentAnalysisPage({
   };
 
   const analyzeSentiment = async () => {
-    setIsLoading(true); // Start loading
+    setIsLoading(true);
 
     try {
       const response = await fetch("/api/sentimentanalysis", {
@@ -49,19 +151,18 @@ export default function SentimentAnalysisPage({
       console.error("Error fetching sentiment analysis:", error);
       setSentiment("Error analyzing sentiment");
     } finally {
-      setIsLoading(false); // End loading
+      setIsLoading(false);
     }
   };
 
   useEffect(() => {
     if (sentiment) {
-      // Only trigger this effect when sentiment is updated
       let sentimentMessage = "";
 
       if (sentiment === "POSITIVE") {
         sentimentMessage =
           "You seem happy; here's a happy song and image for you.";
-        playMusic("happy", "analysis"); // Play music directly
+        playMusic("happy", "analysis");
       } else if (sentiment === "NEGATIVE") {
         sentimentMessage =
           "It seems you are not happy; here's a sad song and image matching your feelings.";
@@ -72,26 +173,26 @@ export default function SentimentAnalysisPage({
         playMusic("calm", "analysis");
       }
 
-      onSentimentAnalyzed(sentimentMessage); // Set message only once after sentiment is analyzed
+      onSentimentAnalyzed(sentimentMessage);
     }
-  }, [sentiment, playMusic, onSentimentAnalyzed]); // Trigger effect when sentiment is updated
+  }, [sentiment, playMusic, onSentimentAnalyzed]);
 
   return (
-    <div className="p-5 max-w-3xl mx-auto grid justify-items-stretch ">
-      <div className="relative flex items-start">
+    <div className="p-4 max-w-3xl mx-auto grid gap-4">
+      <div className="relative flex flex-col md:flex-row items-start md:items-center space-y-4 md:space-y-0">
         <textarea
           value={text}
           onChange={handleTextChange}
           placeholder="Enter text to analyze your sentiment"
           rows={4}
-          className="flex-grow tracking-wide p-3 border border-gray-300 rounded mb-4 ml-6 resize-none w-full" // Make textarea take full width
+          className="flex-grow tracking-wide p-3 border border-gray-300 rounded resize-none w-full"
         />
-        <div className="absolute bottom-1 right-2 text-gray-500 text-sm">
+        <div className="absolute bottom-1 right-4 text-gray-500 text-sm">
           {maxChars - text.length}
         </div>
         <button
           onClick={analyzeSentiment}
-          className="bg-[#326ed1] text-white py-2 px-4 rounded-full hover:bg-[#2758a8] transition-colors ml-8"
+          className="bg-[#326ed1] text-white py-2 px-6 rounded-full hover:bg-[#2758a8] transition-colors mt-4 md:mt-0 md:ml-6 w-full md:w-auto"
           disabled={isLoading}
         >
           {isLoading ? "Checking..." : "Check my mood"}
