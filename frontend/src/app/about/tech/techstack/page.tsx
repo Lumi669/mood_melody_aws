c9688b3432c34f461048a03cb13941742c4402ab
cmjiwa <wangjinghuan@yahoo.com>
2024-10-02 00:15:10 +0300

    Change .webp file of tech stack, better spacing between icons and styling of page

--- frontend/src/app/about/tech/techstack/page.tsx
+++ frontend/src/app/about/tech/techstack/page.tsx
@@ -1,15 +1,25 @@
+"use client";
+
 import Image from "next/image";
 
+import { useMedia } from "@context/MediaContext";
+import { getTextColor } from "@utils/getTextColor";
+
 const TechStackPage = () => {
+  const { isRed, isBlue, isBrown } = useMedia();
+
+  const textColor = getTextColor(isRed, isBlue, isBrown);
   return (
     <div className="h-screen overflow-y-scroll pt-12 pb-80">
       <div className="max-w-5xl mx-auto">
-        <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-4xl font-bold mb-12 text-center">
+        <h1
+          className={`text-2xl sm:text-3xl md:text-4xl lg:text-4xl font-bold mb-12 text-center ${textColor}`}
+        >
           Tech Stack of My Full-Stack Application
         </h1>
         <div className="w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg xl:max-w-2xl mx-auto">
           <Image
-            src="/techstack-border50.webp"
+            src="/tech-stack-border50.webp"
             alt="Tech Stack"
             width={600}
             height={400}
