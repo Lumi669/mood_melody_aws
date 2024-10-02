46900fc8df7fc97fdb8c09174f00c825929d2a98
cmjiwa <wangjinghuan@yahoo.com>
2024-10-02 22:47:28 +0300

    Locally I can save user input in contact form into aws dynamodb database

--- backend/src/routes/saveUserFeedback.ts
+++ backend/src/routes/saveUserFeedback.ts
@@ -5,32 +5,41 @@ import { saveToDynamodbService } from "../services/saveToDynamodbService";
 
 // Handle preflight OPTIONS request
 router.options("/", (req, res) => {
+  console.log("oooooooo OPTIONS request received. Headers: ", req.headers);
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  console.log("oooooooo OPTIONS headers set. Sending 200 response.");
   res.sendStatus(200); // Respond to preflight request with a 200 status
 });
 
+// Handle POST request
 router.post("/", async (req, res) => {
-  console.log("rrrrrr req === ", req);
+  console.log("rrrrrr POST request received. Headers: ", req.headers);
   const userInputs = req.body;
 
-  console.log("uuuuuu userinputs === ", userInputs);
+  console.log("uuuuuu User inputs received: ", userInputs);
 
-  // Add CORS headers for all responses
+  // Add CORS headers for all POST responses
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "POST,OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
+  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
   if (!userInputs || userInputs === undefined) {
+    console.log("Error: No user inputs.");
     return res.status(400).json({ error: "No user inputs" });
   }
 
   try {
+    console.log("Saving to DynamoDB...");
     const message = await saveToDynamodbService(userInputs);
-    console.log("mmmmm message from saveUserFeedback.ts === ", message);
+    console.log("Success: Data saved to DynamoDB. Message: ", message);
     res.json({ "message from aws DynamoDB is": message });
   } catch (error) {
+    console.log(
+      "Error: Failed to save user inputs to DynamoDB. Error: ",
+      error,
+    );
     res.status(500).json({ error: "Failed to save user inputs" });
   }
 });
