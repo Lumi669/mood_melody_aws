bb4a3bde474cf75117ab4b8696285bfd14b63e56
cmjiwa <wangjinghuan@yahoo.com>
2024-10-04 17:24:43 +0300

    Modify functions of retrieve music author and name

--- frontend/utils/extractMusicInfo.ts
+++ frontend/utils/extractMusicInfo.ts
@@ -1,9 +1,10 @@
 export const extractAuthorName = (fileName: string) => {
-  let authorName = fileName.split("-")[0];
+  // Split at the first underscore and take the first part (before "_")
+  let authorName = fileName.split("_")[0];
 
   if (authorName) {
-    // Replace underscores with spaces and trim the result
-    authorName = authorName.replace(/_/g, " ").trim();
+    // Replace hyphens with spaces and trim the result
+    authorName = authorName.replace(/-/g, " ").trim();
     console.log("authorName.trim === ", authorName);
     return authorName;
   } else {
@@ -12,7 +13,7 @@ export const extractAuthorName = (fileName: string) => {
 };
 
 export const extractMusicName = (fileName: string) => {
-  // Splits at the first underscore and takes the second part
+  // Split at the first underscore and take the second part (after "_")
   let songName = fileName.split("_")[1];
 
   if (songName) {
