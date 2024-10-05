87d8b5d9e9791fa9cd7b4eec0b1b24717594272f
cmjiwa <wangjinghuan@yahoo.com>
2024-10-05 14:00:48 +0300

    Test Deploy-DynamoDB-table stage again after changing the .yaml to .yml of the template for dynamodb

--- backend/src/routes/testRoutes.ts
+++ backend/src/routes/testRoutes.ts
@@ -4,7 +4,7 @@ const router = Router();
 
 router.get("/", async (req, res, next) => {
   try {
-    const message = "Test is working ...";
+    const message = "Test is working ...test";
     res.status(200).json(message);
   } catch (error) {
     next(error); // Pass the error to the errorHandler middleware
