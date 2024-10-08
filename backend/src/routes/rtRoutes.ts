99fd83bd9bade8fe2ebfeee6858bf97f686707f0
cmjiwa <wangjinghuan@yahoo.com>
2024-10-08 11:31:25 +0300

    Delete Retain from dynamodb cloudformation, becaue delete protection is better in this case

--- backend/src/routes/rtRoutes.ts
+++ backend/src/routes/rtRoutes.ts
@@ -4,7 +4,7 @@ const router = Router();
 
 router.get("/", async (req, res, next) => {
   try {
-    const message = "Welcome to the backend of the mood-melody app";
+    const message = "Welcome to the backend of the mood-melody app .......";
     res.status(200).json(message);
   } catch (error) {
     next(error); // Pass the error to the errorHandler middleware
