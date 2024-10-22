eff8b9ad4d5920dbeb879f9efe48e687c6280d7b
cmjiwa <wangjinghuan@yahoo.com>
2024-10-22 21:40:46 +0300

    Modify middleware.ts

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -2,12 +2,12 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(req: NextRequest) {
-  const { pathname } = req.nextUrl;
+  const url = req.nextUrl.clone();
 
   // Check if the URL starts with /dev or /prod
-  if (pathname.startsWith("/dev") || pathname.startsWith("/prod")) {
-    // Do nothing, continue with the current path
-    return NextResponse.next();
+  if (url.pathname.startsWith("/dev") || url.pathname.startsWith("/prod")) {
+    // Rewrite the request to the same path without removing the stage prefix
+    return NextResponse.rewrite(url);
   }
 
   // If the path doesn't start with /dev or /prod, continue with the request
